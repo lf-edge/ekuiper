f6e7bb0f310f4561ce557c95a83d21c7ddf67177
ngjaying <ngjaying@gmail.com>
2019-12-12 10:23:49 +0800

    feat(sink): rest sink add data template support

---
+++ common/templates/funcs.go
@@ -0,0 +1,14 @@
+package templates
+
+import (
+	"encoding/json"
+)
+
+//Use the name json in func map
+func JsonMarshal(v interface {}) (string, error) {
+	if a, err := json.Marshal(v); err != nil{
+		return "", err
+	}else{
+		return string(a), nil
+	}
+}
