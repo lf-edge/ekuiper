3e8e0cf15945369977fa3b901d19f2d849e46f19
ngjaying <ngjaying@gmail.com>
2019-12-17 16:01:48 +0800

    feat(prometheus): add metrics to prometheus

--- xstream/nodes/stats_manager.go
+++ xstream/nodes/stats_manager.go
@@ -2,7 +2,11 @@ package nodes
 
 import (
 	"fmt"
+	"github.com/emqx/kuiper/common"
 	"github.com/emqx/kuiper/xstream/api"
+	"github.com/emqx/kuiper/xstream/contexts"
+	"github.com/prometheus/client_golang/prometheus"
+	"strconv"
 	"time"
 )
 
@@ -15,6 +19,13 @@ type StatManager struct {
 	processLatency  int64
 	lastInvocation  time.Time
 	bufferLength    int64
+	//prometheus metrics
+	pTotalRecordsIn  prometheus.Counter
+	pTotalRecordsOut prometheus.Counter
+	pTotalExceptions prometheus.Counter
+	pProcessLatency  prometheus.Gauge
+	pLastInvocation  prometheus.Gauge
+	pBufferLength	 prometheus.Gauge
 	//configs
 	opType           string //"source", "op", "sink"
 	prefix           string
@@ -22,6 +33,8 @@ type StatManager struct {
 	opId             string
 	instanceId		 int
 }
+
+var labelNames = []string{ "rule", "type", "op", "instance" }
 
 const RecordsInTotal = "records_in_total"
 const RecordsOutTotal = "records_out_total"
@@ -33,6 +46,10 @@ const BufferLength   = "buffer_length"
 var MetricNames = []string{RecordsInTotal, RecordsOutTotal, ExceptionsTotal, ProcessLatencyMs, BufferLength, LastInvocation}
 
 func NewStatManager(opType string, ctx api.StreamContext) (*StatManager, error) {
+	r, ok := ctx.Value(contexts.CollectorRegistry).(prometheus.Registry)
+	if !ok {
+		return nil, fmt.Errorf("cannot find prometheus registry in context")
+	}
 	var prefix string
 	switch opType {
 	case "source":
@@ -44,25 +61,62 @@ func NewStatManager(opType string, ctx api.StreamContext) (*StatManager, error) 
 	default:
 		return nil, fmt.Errorf("invalid opType %s, must be \"source\", \"sink\" or \"op\"", opType)
 	}
+	//prometheus initialization
+	totalRecordsIn := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: prefix + RecordsInTotal,
+		Help: "Total number of messages received by the operation",
+	}, labelNames)
+	totalRecordsOut := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: prefix + RecordsOutTotal,
+		Help: "Total number of messages published by the operation",
+	}, labelNames)
+	totalExceptions := prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: prefix + ExceptionsTotal,
+		Help: "Total number of user exceptions",
+	}, labelNames)
+	processLatency := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + ProcessLatencyMs,
+		Help: "Process latency in millisecond",
+	}, labelNames)
+	lastInvocation := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + LastInvocation,
+		Help: "The timestamp of the last invocation of the operation",
+	}, labelNames)
+	bufferLength := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: prefix + BufferLength,
+		Help: "The length of the plan buffer which is shared by all instances",
+	}, labelNames)
+	r.MustRegister(totalRecordsIn, totalRecordsOut, totalExceptions, processLatency, lastInvocation, bufferLength)
 	sm := &StatManager{
 		opType: opType,
 		prefix: prefix,
 		opId:   ctx.GetOpId(),
 		instanceId: ctx.GetInstanceId(),
 	}
+	//assign prometheus
+	strInId := strconv.Itoa(ctx.GetInstanceId())
+	sm.pTotalRecordsIn = totalRecordsIn.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
+	sm.pTotalRecordsOut = totalRecordsOut.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
+	sm.pTotalExceptions = totalExceptions.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
+	sm.pProcessLatency = processLatency.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
+	sm.pLastInvocation = lastInvocation.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
+	sm.pBufferLength   = bufferLength.WithLabelValues(ctx.GetRuleId(), opType, ctx.GetOpId(), strInId)
 	return sm, nil
 }
 
 func (sm *StatManager) IncTotalRecordsIn() {
 	sm.totalRecordsIn++
+	sm.pTotalRecordsIn.Inc()
 }
 
 func (sm *StatManager) IncTotalRecordsOut() {
 	sm.totalRecordsOut++
+	sm.pTotalRecordsOut.Inc()
 }
 
 func (sm *StatManager) IncTotalExceptions() {
 	sm.totalExceptions++
+	sm.pTotalExceptions.Inc()
 	var t time.Time
 	sm.processTimeStart = t
 }
@@ -70,16 +124,19 @@ func (sm *StatManager) IncTotalExceptions() {
 func (sm *StatManager) ProcessTimeStart() {
 	sm.lastInvocation = time.Now()
 	sm.processTimeStart = sm.lastInvocation
+	sm.pLastInvocation.Set(float64(common.TimeToUnixMilli(sm.lastInvocation)))
 }
 
 func (sm *StatManager) ProcessTimeEnd() {
 	if !sm.processTimeStart.IsZero() {
 		sm.processLatency = int64(time.Since(sm.processTimeStart) / time.Millisecond)
+		sm.pProcessLatency.Set(float64(sm.processLatency))
 	}
 }
 
 func (sm *StatManager) SetBufferLength(l int64) {
 	sm.bufferLength = l
+	sm.pBufferLength.Set(float64(l))
 }
 
 func (sm *StatManager) GetMetrics() []interface{} {
