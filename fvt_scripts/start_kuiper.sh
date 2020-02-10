bb04bc8f2d4d8f4e1137ee86cb558302273d0a05
RockyJin <fahua.jin@gmail.com>
2020-02-10 22:10:43 +0800

    fix script issue

--- fvt_scripts/start_kuiper.sh
+++ fvt_scripts/start_kuiper.sh
@@ -5,9 +5,9 @@ os=`uname -s | tr "[A-Z]" "[a-z]"`
 base_dir=_build/kuiper-"$ver"-"$os"-x86_64
 
 rm -rf $base_dir/data/*
-rm -rf $base_dir/log/*
-touch $base_dir/log/kuiper.out
 ls -l $base_dir/bin/server
 
 echo "starting kuiper at " $base_dir
-nohup $base_dir/bin/server > $base_dir/log/kuiper.out 2>&1 &
+cd $base_dir/
+touch log/kuiper.out
+nohup bin/server > log/kuiper.out 2>&1 &
