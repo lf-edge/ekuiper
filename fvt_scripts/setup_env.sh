a2361410a72eef06ec07d81aa5fd69043f679b0a
RockyJin <fahua.jin@gmail.com>
2020-02-10 20:41:44 +0800

    shell script for prepare env

---
+++ fvt_scripts/setup_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+emqx_ids=`ps aux|grep "emqx" | grep "/usr/bin"|awk '{printf $2 " "}'`
+if [ "$emqx_ids" = "" ] ; then
+  echo "No emqx broker was started"
+else
+  for pid in $emqx_ids ; do
+    echo "kill emqx: " $pid
+    kill -9 $pid
+  done
+fi
+echo "starting emqx..."
+systemctl start emqx
+
+
+pids=`ps aux|grep "server" | grep "bin"|awk '{printf $2 " "}'`
+if [ "$pids" = "" ] ; then
+   echo "No kuiper server was started"
+else
+  for pid in $pids ; do
+    echo "kill kuiper " $pid
+    kill -9 $pid
+  done
+fi
+
+rm -rf data/*
+echo "starting kuiper"
+nohup bin/server  > log/kuiper.out 2>&1 &
