76f8813679d8873fa06324bb69907d98158d7529
RockyJin <fahua.jin@gmail.com>
2020-02-11 21:43:18 +0800

    fix file location issue

--- fvt_scripts/README.md
+++ fvt_scripts/README.md
@@ -1,10 +1,7 @@
 
 ## Run JMeter script in local environment
- - Specify the ``base`` property in the JMeter command line, below is an example.
+ - Specify the ``base`` property in the JMeter command line.
+ - Specify the ``fvt`` property in the JMeter command line, below is an example.
     ```
-    bin/jmeter.sh -Dbase="/Users/rockyjin/Downloads/workspace/edge/src/kuiper/_build/kuiper-0.1.1-57-g41ea41b-darwin-x86_64"
-    ```
- - Copy ``rule1.txt`` into directory specified in last step
-    ```
-    cp $kuiper_ws/fvt_scripts/rule1.txt $base
+    bin/jmeter.sh -Dbase="/Users/rockyjin/Downloads/workspace/edge/src/kuiper/_build/kuiper-0.1.1-57-g41ea41b-darwin-x86_64" -Dfvt="/Users/rockyjin/Downloads/workspace/edge/src/kuiper"
     ```
--- fvt_scripts/run_jmeter.sh
+++ fvt_scripts/run_jmeter.sh
@@ -3,9 +3,10 @@
 ver=`git describe --tags --always`
 os=`uname -s | tr "[A-Z]" "[a-z]"`
 base_dir=_build/kuiper-"$ver"-"$os"-x86_64
+fvt_dir=`pwd`
 
 rm -rf *.jtl
 
-/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/streams_test.jmx -Dbase="$base_dir" -l stream_test.jtl
+/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/streams_test.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l stream_test.jtl
 
-/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/rule_test.jmx -Dbase="$base_dir" -l rule_test.jtl
+/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/rule_test.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l rule_test.jtl
