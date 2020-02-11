9d24a9a6ffe0327f6ec3472b4c9a913079c5e1c9
RockyJin <fahua.jin@gmail.com>
2020-02-11 21:32:42 +0800

    add rule cli script & fix one problem

--- fvt_scripts/run_jmeter.sh
+++ fvt_scripts/run_jmeter.sh
@@ -4,6 +4,8 @@ ver=`git describe --tags --always`
 os=`uname -s | tr "[A-Z]" "[a-z]"`
 base_dir=_build/kuiper-"$ver"-"$os"-x86_64
 
+rm -rf *.jtl
+
 /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/streams_test.jmx -Dbase="$base_dir" -l stream_test.jtl
 
 /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/rule_test.jmx -Dbase="$base_dir" -l rule_test.jtl
