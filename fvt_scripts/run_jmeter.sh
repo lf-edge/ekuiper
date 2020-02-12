95bd511a591229ccaabf7d8aaa2568fc29b097f5
RockyJin <fahua.jin@gmail.com>
2020-02-12 11:40:14 +0800

    add simple rule testcase

--- fvt_scripts/run_jmeter.sh
+++ fvt_scripts/run_jmeter.sh
@@ -10,3 +10,5 @@ rm -rf *.jtl
 /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/streams_test.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l stream_test.jtl
 
 /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/rule_test.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l rule_test.jtl
+
+/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/simple_rule.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l simple_rule.jtl
