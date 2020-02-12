bf07c9ff9e0b5bad3c8f4a498888b807262cfc5e
RockyJin <fahua.jin@gmail.com>
2020-02-12 17:38:35 +0800

    setup jmeter files

--- fvt_scripts/run_jmeter.sh
+++ fvt_scripts/run_jmeter.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+function downloadjar
+{
+  if [ ! -f $1 ];then
+    wget -O $1 $2
+  else
+    echo "Already downloaded $1."
+  fi
+}
+
+downloadjar "/opt/jmeter/lib/json-lib-2.4-jdk15.jar" https://repo1.maven.org/maven2/net/sf/json-lib/json-lib/2.4/json-lib-2.4-jdk15.jar
+downloadjar "/opt/jmeter/lib/commons-beanutils-1.8.0.jar" https://repo1.maven.org/maven2/commons-beanutils/commons-beanutils/1.8.0/commons-beanutils-1.8.0.jar
+downloadjar "/opt/jmeter/lib/commons-collections-3.2.1.jar" https://repo1.maven.org/maven2/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar
+downloadjar "/opt/jmeter/lib/commons-lang-2.5.jar" https://repo1.maven.org/maven2/commons-lang/commons-lang/2.5/commons-lang-2.5.jar
+downloadjar "/opt/jmeter/lib/commons-logging-1.1.1.jar" https://repo1.maven.org/maven2/commons-logging/commons-logging/1.1.1/commons-logging-1.1.1.jar
+downloadjar "/opt/jmeter/lib/ezmorph-1.0.6.jar" https://repo1.maven.org/maven2/net/sf/ezmorph/ezmorph/1.0.6/ezmorph-1.0.6.jar
+
 ver=`git describe --tags --always`
 os=`uname -s | tr "[A-Z]" "[a-z]"`
 base_dir=_build/kuiper-"$ver"-"$os"-x86_64
--- fvt_scripts/setup_env.sh
+++ fvt_scripts/setup_env.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-json_lib="/opt/jmeter/lib/json-lib-2.4-jdk15.jar"
-if [ ! -f $json_lib ];then
-   wget -O $json_lib https://repo1.maven.org/maven2/net/sf/json-lib/json-lib/2.4/json-lib-2.4-jdk15.jar
-else
-   echo "Already downloaded $json_lib."
-fi
-
 emqx_ids=`ps aux|grep "emqx" | grep "/usr/bin"|awk '{printf $2 " "}'`
 if [ "$emqx_ids" = "" ] ; then
   echo "No emqx broker was started"
@@ -19,7 +12,6 @@ else
     #kill -9 $pid
   #done
 fi
-
 
 pids=`ps aux|grep "server" | grep "bin"|awk '{printf $2 " "}'`
 if [ "$pids" = "" ] ; then
