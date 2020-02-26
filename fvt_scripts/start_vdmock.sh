3be244d88e22c915b94cb37508ea113b0250e6c0
RockyJin <fahua.jin@gmail.com>
2020-02-26 12:51:35 +0800

    move vd file location

--- fvt_scripts/start_vdmock.sh
+++ fvt_scripts/start_vdmock.sh
@@ -5,7 +5,7 @@
 echo "starting edgex value descriptor mockup server."
 PATH=$PATH:/usr/local/go/bin
 export PATH
-go build -o fvt_scripts/edgex/vdmocker fvt_scripts/edgex/vd_server.go
+go build -o fvt_scripts/edgex/valuedesc/vdmocker fvt_scripts/edgex/valuedesc/vd_server.go
 go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
 export BUILD_ID=dontKillMe
 nohup fvt_scripts/edgex/vdmocker > vdmocker.out 2>&1 &
