a7bc240e5bc1b4776ff5125bc0e4d6c99f6eb49e
RockyJin <fahua.jin@gmail.com>
2020-02-26 16:11:37 +0800

    add execute permission

--- fvt_scripts/start_vdmock.sh
+++ fvt_scripts/start_vdmock.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 echo "starting edgex value descriptor mockup server."
+rm -rf fvt_scripts/edgex/valuedesc/vdmocker
+rm -rf fvt_scripts/edgex/pub
 go build -o fvt_scripts/edgex/valuedesc/vdmocker fvt_scripts/edgex/valuedesc/vd_server.go
 go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
+
+chmod +x fvt_scripts/edgex/valuedesc/vdmocker
+chmod +x fvt_scripts/edgex/pub
+
 export BUILD_ID=dontKillMe
 nohup fvt_scripts/edgex/valuedesc/vdmocker > vdmocker.out 2>&1 &
