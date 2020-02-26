292bf7cf6e5533dbea790f42ba8a624dd2be9361
RockyJin <fahua.jin@gmail.com>
2020-02-26 15:39:34 +0800

    change vdmock path

--- fvt_scripts/start_vdmock.sh
+++ fvt_scripts/start_vdmock.sh
@@ -4,4 +4,4 @@ echo "starting edgex value descriptor mockup server."
 go build -o fvt_scripts/edgex/valuedesc/vdmocker fvt_scripts/edgex/valuedesc/vd_server.go
 go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
 export BUILD_ID=dontKillMe
-nohup fvt_scripts/edgex/vdmocker > vdmocker.out 2>&1 &
+nohup fvt_scripts/edgex/valuedesc/vdmocker > vdmocker.out 2>&1 &
