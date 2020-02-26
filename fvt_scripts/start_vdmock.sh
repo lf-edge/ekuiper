f0c18493f9642599fd00215cce6ea0371dec1636
zhanghongtong <zhanghongtong@foxmail.com>
2020-02-26 15:30:58 +0800

    Update

--- fvt_scripts/start_vdmock.sh
+++ fvt_scripts/start_vdmock.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-echo "starting edgex value descriptor mockup server."
+
 go build -o fvt_scripts/edgex/valuedesc/vdmocker fvt_scripts/edgex/valuedesc/vd_server.go
 go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
 export BUILD_ID=dontKillMe
 nohup fvt_scripts/edgex/vdmocker > vdmocker.out 2>&1 &
+echo "starting edgex value descriptor mockup server."
