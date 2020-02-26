68f139fd90f9f1d1f7535fe8c5fba4ae665dd020
zhanghongtong <zhanghongtong@foxmail.com>
2020-02-26 14:46:17 +0800

    Fix build error

--- fvt_scripts/start_vdmock.sh
+++ fvt_scripts/start_vdmock.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-
-
 echo "starting edgex value descriptor mockup server."
-PATH=$PATH:/usr/local/go/bin
-export PATH
 go build -o fvt_scripts/edgex/valuedesc/vdmocker fvt_scripts/edgex/valuedesc/vd_server.go
 go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
 export BUILD_ID=dontKillMe
