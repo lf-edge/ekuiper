85fe6908d81023a07b88424fc0671f7f5dba3f82
RockyJin <fahua.jin@gmail.com>
2020-03-12 20:01:22 +0800

    add select_edgex_condition_rule.jmx script back

--- fvt_scripts/run_jmeter.sh
+++ fvt_scripts/run_jmeter.sh
@@ -68,8 +68,8 @@ echo -e "---------------------------------------------\n"
 echo -e "---------------------------------------------\n"
 
 if test $with_edgex = true; then
-  #/opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/select_edgex_condition_rule.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l jmeter_logs/select_edgex_condition_rule.jtl -j jmeter_logs/select_edgex_condition_rule.log
-  #echo -e "---------------------------------------------\n"
+  /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/select_edgex_condition_rule.jmx -Dbase="$base_dir" -Dfvt="$fvt_dir" -l jmeter_logs/select_edgex_condition_rule.jtl -j jmeter_logs/select_edgex_condition_rule.log
+  echo -e "---------------------------------------------\n"
 
   /opt/jmeter/bin/jmeter.sh -Jjmeter.save.saveservice.output_format=xml -n -t fvt_scripts/select_edgex_another_bus_rule.jmx -Dfvt="$fvt_dir" -l jmeter_logs/select_edgex_another_bus_rule.jtl -j jmeter_logs/select_edgex_another_bus_rule.log
   echo -e "---------------------------------------------\n"
