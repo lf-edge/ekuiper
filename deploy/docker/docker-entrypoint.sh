d48350b6a796f80aa87d4fdf38f6a8f031f14265
zhanghongtong <zhanghongtong@foxmail.com>
2020-03-20 11:38:56 +0800

    Update docker-entrypoint.sh

--- deploy/docker/docker-entrypoint.sh
+++ deploy/docker/docker-entrypoint.sh
@@ -12,43 +12,75 @@ CONFIG="$KUIPER_HOME/etc/mqtt_source.yaml"
 if [ ! -z "$MQTT_BROKER_ADDRESS" ]; then
     sed -i '/default:/ ,/servers/{/servers/d}' $CONFIG
     sed -i "/default:/a\  servers: [$MQTT_BROKER_ADDRESS]" $CONFIG
-    echo "default.servers = $MQTT_BROKER_ADDRESS"
+    echo "mqtt.default.servers = $MQTT_BROKER_ADDRESS"
 fi
 
 if [ ! -z "$MQTT_BROKER_SHARED_SUBSCRIPTION" ]; then
     sed -i '/default:/ ,/sharedSubscription/{/sharedSubscription/d}' $CONFIG
     sed -i "/default:/a\  sharedSubscription: $MQTT_BROKER_SHARED_SUBSCRIPTION" $CONFIG
-    echo "default.sharedSubscription = $MQTT_BROKER_SHARED_SUBSCRIPTION"
+    echo "mqtt.default.sharedSubscription = $MQTT_BROKER_SHARED_SUBSCRIPTION"
 fi
 
 if [ ! -z "$MQTT_BROKER_QOS" ]; then
     sed -i '/default:/ ,/qos/{/qos/d}' $CONFIG
     sed -i "/default:/a\  qos: $MQTT_BROKER_QOS" $CONFIG
-    echo "default.qos = $MQTT_BROKER_QOS"
+    echo "mqtt.default.qos = $MQTT_BROKER_QOS"
 fi
 
 if [ ! -z "$MQTT_BROKER_USERNAME" ]; then
     sed -i '/default:/ ,/username/{/username/d}' $CONFIG
     sed -i "/default:/a\  username: $MQTT_BROKER_USERNAME" $CONFIG
-    echo "default.username = $MQTT_BROKER_USERNAME"
+    echo "mqtt.default.username = $MQTT_BROKER_USERNAME"
 fi
 
 if [ ! -z "$MQTT_BROKER_PASSWORD" ]; then
     sed -i '/default:/ ,/password/{/password/d}' $CONFIG
     sed -i "/default:/a\  password: $MQTT_BROKER_PASSWORD" $CONFIG
-    echo "default.password = $MQTT_BROKER_PASSWORD"
+    echo "mqtt.default.password = $MQTT_BROKER_PASSWORD"
 fi
 
 if [ ! -z "$MQTT_BROKER_CER_PATH" ]; then
     sed -i '/default:/ ,/certificationPath/{/certificationPath/d}' $CONFIG
     sed -i "/default:/a\  certificationPath: $MQTT_BROKER_CER_PATH" $CONFIG
-    echo "default.certificationPath = $MQTT_BROKER_CER_PATH"
+    echo "mqtt.default.certificationPath = $MQTT_BROKER_CER_PATH"
 fi
 
 if [ ! -z "$MQTT_BROKER_KEY_PATH" ]; then
     sed -i '/default:/ ,/privateKeyPath/{/privateKeyPath/d}' $CONFIG
     sed -i "/default:/a\  privateKeyPath: $MQTT_BROKER_KEY_PATH" $CONFIG
-    echo "default.privateKeyPath = $MQTT_BROKER_KEY_PATH"
+    echo "mqtt.default.privateKeyPath = $MQTT_BROKER_KEY_PATH"
+fi
+
+EDGEX_CONFIG="$KUIPER_HOME/etc/sources/edgex.yaml"
+
+if [ ! -z "$EDGEX_PROTOCOL" ]; then
+    sed -i '/default:/ ,/protocol/{/protocol/d}' $EDGEX_CONFIG
+    sed -i "/default:/a\  protocol: [$EDGEX_PROTOCOL]" $EDGEX_CONFIG
+    echo "edgex.default.protocol = $EDGEX_PROTOCOL"
+fi
+
+if [ ! -z "$EDGEX_SERVER" ]; then
+    sed -i '/default:/ ,/server/{/server/d}' $EDGEX_CONFIG
+    sed -i "/default:/a\  server: $EDGEX_SERVER" $EDGEX_CONFIG
+    echo "edgex.default.server = $EDGEX_SERVER"
+fi
+
+if [ ! -z "$EDGEX_PORT" ]; then
+    sed -i '/default:/ ,/port/{/port/d}' $EDGEX_CONFIG
+    sed -i "/default:/a\  port: $EDGEX_PORT" $EDGEX_CONFIG
+    echo "edgex.default.port = $EDGEX_PORT"
+fi
+
+if [ ! -z "$EDGEX_TOPIC" ]; then
+    sed -i '/default:/ ,/topic/{/topic/d}' $EDGEX_CONFIG
+    sed -i "/default:/a\  topic: $EDGEX_TOPIC" $CONFIEDGEX_CONFIGG
+    echo "edgex.default.topic = $EDGEX_TOPIC"
+fi
+
+if [ ! -z "$EDGEX_SERVICE_SERVER" ]; then
+    sed -i '/default:/ ,/serviceServer/{/serviceServer/d}' $EDGEX_CONFIG
+    sed -i "/default:/a\  serviceServer: $EDGEX_SERVICE_SERVER" $CEDGEX_CONFIG
+    echo "edgex.default.serviceServer = $EDGEX_SERVICE_SERVER"
 fi
 
 exec "$@"
