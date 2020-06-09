41e6563656bf1e6dff1007b6418c890f3a4ebba4
ngjaying <ngjaying@gmail.com>
2020-06-09 10:13:20 +0800

    bug(rule): get rule state error for runtime invalid rule

--- xstream/server/server/ruleManager.go
+++ xstream/server/server/ruleManager.go
@@ -43,15 +43,15 @@ func (rr *RuleRegistry) Delete(key string) {
 }
 
 func createRuleState(rule *api.Rule) (*RuleState, error) {
+	rs := &RuleState{
+		Name: rule.Id,
+	}
+	registry.Store(rule.Id, rs)
 	if tp, err := ruleProcessor.ExecInitRule(rule); err != nil {
-		return nil, err
+		return rs, err
 	} else {
-		rs := &RuleState{
-			Name:      rule.Id,
-			Topology:  tp,
-			Triggered: true,
-		}
-		registry.Store(rule.Id, rs)
+		rs.Topology = tp
+		rs.Triggered = true
 		return rs, nil
 	}
 }
@@ -100,7 +100,7 @@ func getRuleState(name string) (string, error) {
 func doGetRuleState(rs *RuleState) (string, error) {
 	result := ""
 	if !rs.Triggered {
-		result = "Stopped: canceled manually."
+		result = "Stopped: canceled manually or by error."
 		return result, nil
 	}
 	c := (*rs.Topology).GetContext()
@@ -175,7 +175,7 @@ func startRule(name string) error {
 }
 
 func stopRule(name string) (result string) {
-	if rs, ok := registry.Load(name); ok {
+	if rs, ok := registry.Load(name); ok && rs.Triggered {
 		(*rs.Topology).Cancel()
 		rs.Triggered = false
 		result = fmt.Sprintf("Rule %s was stopped.", name)
