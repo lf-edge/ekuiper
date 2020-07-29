4fa468c4c4d90869f5cabeb70ea052ac6c287e64
mayuedong <mayuedong@emx.io>
2020-07-29 16:36:01 +0800

    feat(signal): Add catch signal

--- tools/kubernetes/main.go
+++ tools/kubernetes/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"github.com/emqx/kuiper/tools/kubernetes/util"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+		<-sigint
+		os.Exit(0)
+	}()
 	util.Process()
 }
