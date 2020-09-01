09499c41a808377e488d26970a2e291806b9d3fa
EMqmyd <mayuedong@emqx.io>
2020-09-01 09:36:15 +0800

    stash

---
+++ plugins/funcMeta.go
@@ -0,0 +1,25 @@
+package plugins
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/emqx/kuiper/common"
+	"io/ioutil"
+	"path"
+	"reflect"
+	"strings"
+)
+
+
+
+
+
+{
+   "name": "path",
+   "control": "text",
+   "eximple": "text",
+   "type": "string",
+   "hint": {
+     "en_US": "The file path for saving the result",
+     "zh_CN": "保存结果的文件路径"
+   }
+ }
