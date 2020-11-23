b5a092f3cf002245eb9cdef4e5b99267115a46e0
EMqmyd <mayuedong@emx.io>
2020-11-23 20:21:19 +0800

    feat(addr):No way to limit client and REST APIs to localhost

--- common/util.go
+++ common/util.go
@@ -67,7 +67,9 @@ type KuiperConf struct {
 		FileLog        bool     `yaml:"fileLog"`
 		RotateTime     int      `yaml:"rotateTime"`
 		MaxAge         int      `yaml:"maxAge"`
+		Ip             string   `yaml:"ip"`
 		Port           int      `yaml:"port"`
+		RestIp         string   `yaml:"restIp"`
 		RestPort       int      `yaml:"restPort"`
 		RestTls        *tlsConf `yaml:"restTls"`
 		Prometheus     bool     `yaml:"prometheus"`
@@ -128,6 +130,13 @@ func InitConf() {
 		Log.Fatal(err)
 	} else {
 		Config = &kc
+	}
+	if 0 == len(Config.Basic.Ip) {
+		Config.Basic.Ip = "0.0.0.0"
+	}
+	if 0 == len(Config.Basic.RestIp) {
+		Config.Basic.RestIp = "0.0.0.0"
+
 	}
 
 	if Config.Basic.Debug {
