3bb379006fb0e9f708061711c270038b7f1a4f9a
EMqmyd <mayuedong@emx.io>
2020-12-11 15:11:24 +0800

    feat(kv):Migrate data to sqlite.

---
+++ tools/migration/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/emqx/kuiper/tools/migration/util"
+	"log"
+	"os"
+)
+
+func main() {
+	if 2 != len(os.Args) {
+		log.Fatal("Please enter the correct path. For example: ./migration kuiper/bin/data")
+	}
+
+	if err := util.DataMigration(os.Args[1]); nil != err {
+		log.Fatal(err)
+	} else {
+		log.Println("The data migration was successful.")
+	}
+}
