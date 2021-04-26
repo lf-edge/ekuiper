6970fcb9c816ba746d6d39bebd17987edf0393c4
ngjaying <ngjaying@gmail.com>
2021-04-26 18:01:01 +0800

    feat(edgex): update sink

--- xstream/sinks/edgex_sink.go
+++ xstream/sinks/edgex_sink.go
@@ -5,13 +5,13 @@ package sinks
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/edgexfoundry/go-mod-core-contracts/clients/coredata"
-	"github.com/edgexfoundry/go-mod-core-contracts/clients/urlclient/local"
-	"github.com/edgexfoundry/go-mod-core-contracts/models"
-	"github.com/edgexfoundry/go-mod-messaging/messaging"
-	"github.com/edgexfoundry/go-mod-messaging/pkg/types"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2"
+	"github.com/edgexfoundry/go-mod-core-contracts/v2/v2/dtos"
+	"github.com/edgexfoundry/go-mod-messaging/v2/messaging"
+	"github.com/edgexfoundry/go-mod-messaging/v2/pkg/types"
 	"github.com/emqx/kuiper/common"
 	"github.com/emqx/kuiper/xstream/api"
+	"reflect"
 )
 
 type EdgexMsgBusSink struct {
@@ -133,43 +133,44 @@ func (ems *EdgexMsgBusSink) Open(ctx api.StreamContext) error {
 	return nil
 }
 
-func (ems *EdgexMsgBusSink) produceEvents(result []byte) (*models.Event, error) {
+func (ems *EdgexMsgBusSink) produceEvents(ctx api.StreamContext, result []byte) (*dtos.Event, error) {
 	var m []map[string]interface{}
 	if err := json.Unmarshal(result, &m); err == nil {
-		m1, f := ems.getMeta(m)
-		var event = &models.Event{}
-		if f {
-			event.Device = m1.getStrVal("device")
-			event.Created = m1.getIntVal("created")
-			event.Modified = m1.getIntVal("modified")
-			event.Origin = m1.getIntVal("origin")
-			event.ID = m1.getStrVal("id")
-			event.Pushed = m1.getIntVal("pushed")
-		}
+		m1 := ems.getMeta(m)
+		event := m1.createEvent()
 		//Override the devicename if user specified the value
 		if ems.deviceName != "" {
-			event.Device = ems.deviceName
+			event.DeviceName = ems.deviceName
 		}
-
 		for _, v := range m {
 			for k1, v1 := range v {
 				if k1 == ems.metadata {
 					continue
 				} else {
-					value := fmt.Sprintf("%v", v1)
-					r := models.Reading{Name: k1, Value: value}
-					if m, ok := m1[k1]; ok {
-						if mm, ok1 := m.(map[string]interface{}); ok1 {
-							mm1 := meta(mm)
-							r.Created = mm1.getIntVal("created")
-							r.Device = mm1.getStrVal("device")
-							r.Id = mm1.getStrVal("id")
-							r.Modified = mm1.getIntVal("modified")
-							r.Origin = mm1.getIntVal("origin")
-							r.Pushed = mm1.getIntVal("pushed")
+					var (
+						vt string
+						vv interface{}
+					)
+					mm1 := m1.readingMeta(ctx, k1)
+					if mm1 != nil && mm1.valueType != nil {
+						vt = *mm1.valueType
+						vv = v1
+					} else {
+						vt, vv, err = getValueType(v1)
+						if err != nil {
+							ctx.GetLogger().Errorf("%v", err)
+							continue
 						}
 					}
-					event.Readings = append(event.Readings, r)
+					err = event.AddSimpleReading(k1, vt, vv)
+					if err != nil {
+						ctx.GetLogger().Errorf("%v", err)
+						continue
+					}
+					r := event.Readings[len(event.Readings)-1]
+					if mm1 != nil {
+						event.Readings[len(event.Readings)-1] = mm1.decorate(&r)
+					}
 				}
 			}
 		}
@@ -179,62 +180,102 @@ func (ems *EdgexMsgBusSink) produceEvents(result []byte) (*models.Event, error) 
 	}
 }
 
-type meta map[string]interface{}
+func getValueType(v interface{}) (string, interface{}, error) {
+	k := reflect.TypeOf(v).Kind()
+	switch k {
+	case reflect.Bool:
+		return v2.ValueTypeBool, v, nil
+	case reflect.String:
+		return v2.ValueTypeString, v, nil
+	case reflect.Int64:
+		return v2.ValueTypeInt64, v, nil
+	case reflect.Int:
+		return v2.ValueTypeInt64, v, nil
+	case reflect.Float64:
+		return v2.ValueTypeFloat64, v, nil
+	case reflect.Slice:
+		arrayValue, ok := v.([]interface{})
+		if !ok {
+			return "", nil, fmt.Errorf("unable to cast value to []interface{} for %v", v)
+		}
+		if len(arrayValue) > 0 {
+			ka := reflect.TypeOf(arrayValue[0]).Kind()
+			switch ka {
+			case reflect.Bool:
+				result := make([]bool, len(arrayValue))
+				for i, av := range arrayValue {
+					temp, ok := av.(bool)
+					if !ok {
+						return "", nil, fmt.Errorf("unable to cast value to []bool for %v", v)
+					}
+					result[i] = temp
+				}
+				return v2.ValueTypeBoolArray, result, nil
+			case reflect.String:
+				result := make([]string, len(arrayValue))
+				for i, av := range arrayValue {
+					temp, ok := av.(string)
+					if !ok {
+						return "", nil, fmt.Errorf("unable to cast value to []string for %v", v)
+					}
+					result[i] = temp
+				}
+				return v2.ValueTypeStringArray, result, nil
+			case reflect.Int64, reflect.Int:
+				result := make([]int64, len(arrayValue))
+				for i, av := range arrayValue {
+					temp, ok := av.(int64)
+					if !ok {
+						return "", nil, fmt.Errorf("unable to cast value to []int64 for %v", v)
+					}
+					result[i] = temp
+				}
+				return v2.ValueTypeInt64Array, result, nil
+			case reflect.Float64:
+				result := make([]float64, len(arrayValue))
+				for i, av := range arrayValue {
+					temp, ok := av.(float64)
+					if !ok {
+						return "", nil, fmt.Errorf("unable to cast value to []float64 for %v", v)
+					}
+					result[i] = temp
+				}
+				return v2.ValueTypeFloat64Array, result, nil
+			}
+		} else { // default to string array
+			return v2.ValueTypeStringArray, []string{}, nil
+		}
 
-func (ems *EdgexMsgBusSink) getMeta(result []map[string]interface{}) (meta, bool) {
+	}
+	return "", nil, fmt.Errorf("unsupported value %v(%s)", v, k)
+}
+
+func (ems *EdgexMsgBusSink) getMeta(result []map[string]interface{}) *meta {
 	if ems.metadata == "" {
-		return nil, false
+		return newMetaFromMap(nil)
 	}
 	//Try to get the meta field
 	for _, v := range result {
 		if m, ok := v[ems.metadata]; ok {
 			if m1, ok1 := m.(map[string]interface{}); ok1 {
-				return meta(m1), true
+				return newMetaFromMap(m1)
 			} else {
 				common.Log.Infof("Specified a meta field, but the field does not contains any EdgeX metadata.")
 			}
 		}
 	}
-	return nil, false
-}
-
-func (m meta) getIntVal(k string) int64 {
-	if v, ok := m[k]; ok {
-		if v1, ok1 := v.(float64); ok1 {
-			return int64(v1)
-		}
-	}
-	return 0
-}
-
-func (m meta) getStrVal(k string) string {
-	if v, ok := m[k]; ok {
-		if v1, ok1 := v.(string); ok1 {
-			return v1
-		}
-	}
-	return ""
-}
-
-func (ems *EdgexMsgBusSink) getMetaValueAsMap(m meta, k string) map[string]interface{} {
-	if v, ok := m[k]; ok {
-		if v1, ok1 := v.(map[string]interface{}); ok1 {
-			return v1
-		}
-	}
-	return nil
+	return newMetaFromMap(nil)
 }
 
 func (ems *EdgexMsgBusSink) Collect(ctx api.StreamContext, item interface{}) error {
 	logger := ctx.GetLogger()
-	client := coredata.NewEventClient(local.New(""))
 	if payload, ok := item.([]byte); ok {
 		logger.Debugf("EdgeX message bus sink: %s\n", payload)
-		evt, err := ems.produceEvents(payload)
+		evt, err := ems.produceEvents(ctx, payload)
 		if err != nil {
 			return fmt.Errorf("Failed to convert to EdgeX event: %s.", err.Error())
 		}
-		data, err := client.MarshalEvent(*evt)
+		data, err := json.Marshal(evt)
 		if err != nil {
 			return fmt.Errorf("unexpected error MarshalEvent %v", err)
 		}
@@ -260,4 +301,158 @@ func (ems *EdgexMsgBusSink) Close(ctx api.StreamContext) error {
 		}
 	}
 	return nil
+}
+
+type eventMeta struct {
+	id          *string
+	deviceName  string
+	profileName string
+	sourceName  string
+	origin      *int64
+	tags        map[string]string
+}
+
+type readingMeta struct {
+	id           *string
+	deviceName   *string
+	profileName  *string
+	resourceName *string
+	origin       *int64
+	valueType    *string
+}
+
+func (m *readingMeta) decorate(r *dtos.BaseReading) dtos.BaseReading {
+	if m.id != nil {
+		r.Id = *m.id
+	}
+	if m.deviceName != nil {
+		r.DeviceName = *m.deviceName
+	}
+	if m.resourceName != nil {
+		r.ResourceName = *m.resourceName
+	}
+	if m.profileName != nil {
+		r.ProfileName = *m.profileName
+	}
+	if m.origin != nil {
+		r.Origin = *m.origin
+	}
+	if m.valueType != nil {
+		r.ValueType = *m.valueType
+	}
+	return *r
+}
+
+type meta struct {
+	eventMeta
+	readingMetas map[string]interface{}
+}
+
+func newMetaFromMap(m1 map[string]interface{}) *meta {
+	result := &meta{
+		eventMeta: eventMeta{
+			profileName: "kuiper",
+		},
+	}
+	for k, v := range m1 {
+		switch k {
+		case "id":
+			if v1, ok := v.(string); ok {
+				result.id = &v1
+			}
+		case "deviceName":
+			if v1, ok := v.(string); ok {
+				result.deviceName = v1
+			}
+		case "profileName":
+			if v1, ok := v.(string); ok {
+				result.profileName = v1
+			}
+		case "sourceName":
+			if v1, ok := v.(string); ok {
+				result.sourceName = v1
+			}
+		case "origin":
+			if v1, ok := v.(float64); ok {
+				temp := int64(v1)
+				result.origin = &temp
+			}
+		case "tags":
+			if v1, ok1 := v.(map[string]interface{}); ok1 {
+				r := make(map[string]string)
+				for k, vi := range v1 {
+					s, ok := vi.(string)
+					if ok {
+						r[k] = s
+					}
+				}
+				result.tags = r
+			}
+		default:
+			if result.readingMetas == nil {
+				result.readingMetas = make(map[string]interface{})
+			}
+			result.readingMetas[k] = v
+		}
+	}
+	return result
+}
+
+func (m *meta) readingMeta(ctx api.StreamContext, readingName string) *readingMeta {
+	vi, ok := m.readingMetas[readingName]
+	if !ok {
+		return nil
+	}
+	m1, ok := vi.(map[string]interface{})
+	if !ok {
+		ctx.GetLogger().Errorf("reading %s meta is not a map, but %v", readingName, vi)
+		return nil
+	}
+	result := &readingMeta{}
+	for k, v := range m1 {
+		switch k {
+		case "id":
+			if v1, ok := v.(string); ok {
+				result.id = &v1
+			}
+		case "deviceName":
+			if v1, ok := v.(string); ok {
+				result.deviceName = &v1
+			}
+		case "profileName":
+			if v1, ok := v.(string); ok {
+				result.profileName = &v1
+			}
+		case "resourceName":
+			if v1, ok := v.(string); ok {
+				result.resourceName = &v1
+			}
+		case "origin":
+			if v1, ok := v.(float64); ok {
+				temp := int64(v1)
+				result.origin = &temp
+			}
+		case "valueType":
+			if v1, ok := v.(string); ok {
+				result.resourceName = &v1
+			}
+		default:
+			ctx.GetLogger().Warnf("reading %s meta got unknown field %s of value %v", readingName, k, v)
+		}
+	}
+	return result
+}
+
+func (m *meta) createEvent() *dtos.Event {
+	event := dtos.NewEvent(m.profileName, m.deviceName, m.sourceName)
+	if m.id != nil {
+		event.Id = *m.id
+	}
+	if m.origin != nil {
+		event.Origin = *m.origin
+	}
+	if m.tags != nil {
+		event.Tags = m.tags
+	}
+	return &event
 }
