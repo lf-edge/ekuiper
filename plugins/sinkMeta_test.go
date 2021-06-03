1898183eaa67a33e0b373089eaad8bd97146454f
ngjaying <ngjaying@gmail.com>
2021-06-03 10:34:57 +0800

    refactor(uiMeta): return only one meta for get sink meta call
    Signed-off-by: ngjaying <ngjaying@gmail.com>

--- plugins/sinkMeta_test.go
+++ plugins/sinkMeta_test.go
@@ -26,13 +26,12 @@ func TestHintWhenModifySink(t *testing.T) {
 	g_sinkMetadata["taos.json"] = taosMeta
 	g_sinkMetadata["log.json"] = logMeta
 
-	oldSink := new(uiSinks)
-	err := oldSink.hintWhenNewSink("taos")
-	if nil != err {
-		t.Error(err)
-	}
-
-	if "911.911.911.911" != oldSink.CustomProperty["taos"].Fields[0].Default {
-		t.Errorf("fail")
+	oldSink, err := GetSinkMeta("taos", "en_US")
+	if err != nil {
+		t.Errorf("%v", err)
+	} else {
+		if "911.911.911.911" != oldSink.Fields[0].Default {
+			t.Errorf("fail")
+		}
 	}
 }
