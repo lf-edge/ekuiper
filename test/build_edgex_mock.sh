d5fc52f4195cc535098698eb86bd1ae6cbf797ba
ngjaying <ngjaying@gmail.com>
2021-07-01 20:42:11 +0800

    refactor(test): rename fvt scripts directory name
    Signed-off-by: ngjaying <ngjaying@gmail.com>

--- test/build_edgex_mock.sh
+++ test/build_edgex_mock.sh
@@ -2,13 +2,13 @@
 set -e
 echo "building edgex mock pubsub programs."
 
-rm -rf fvt_scripts/edgex/pub
-rm -rf fvt_scripts/edgex/sub/sub
+rm -rf test/edgex/pub
+rm -rf test/edgex/sub/sub
 
-go build -o fvt_scripts/edgex/pub fvt_scripts/edgex/pub.go
-go build -o fvt_scripts/edgex/sub/sub fvt_scripts/edgex/sub/sub.go
+go build -o test/edgex/pub test/edgex/pub.go
+go build -o test/edgex/sub/sub test/edgex/sub/sub.go
 
-chmod +x fvt_scripts/edgex/pub
-chmod +x fvt_scripts/edgex/sub/sub
+chmod +x test/edgex/pub
+chmod +x test/edgex/sub/sub
 
 export BUILD_ID=dontKillMe
