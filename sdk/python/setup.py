724b246474dbcc6309d099bd6104cd4cd67418fa
Jiyong Huang <huangjy@emqx.io>
2021-10-14 17:01:56 +0800

    fix(sdk): setup and package problems
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ekuiper',
     version='0.0.1',
-    packages=['ekuiper'],
+    packages=find_packages(),
     url='https://github.com/lf-edge/ekuiper',
     license='Apache License 2.0',
     author='LF Edge eKuiper team',
     author_email='huangjy@emqx.io',
-    description='Python SDK for eKuiper portable plugin'
+    description='Python SDK for eKuiper portable plugin',
+    install_requires=["pynng"],
 )
