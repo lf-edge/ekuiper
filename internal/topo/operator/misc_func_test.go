74c043426f5a7f6eeaeddf4f6bbb266a72983570
Jiyong Huang <huangjy@emqx.io>
2022-02-22 15:51:10 +0800

    feat(func): implement changed_col function as a scalar func
    Detect the change of a single col or the whole message
    
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/topo/operator/misc_func_test.go
+++ internal/topo/operator/misc_func_test.go
@@ -19,9 +19,12 @@ import (
 	"github.com/lf-edge/ekuiper/internal/conf"
 	"github.com/lf-edge/ekuiper/internal/testx"
 	"github.com/lf-edge/ekuiper/internal/topo/context"
+	"github.com/lf-edge/ekuiper/internal/topo/state"
 	"github.com/lf-edge/ekuiper/internal/xsql"
+	"github.com/lf-edge/ekuiper/pkg/api"
 	"github.com/lf-edge/ekuiper/pkg/cast"
 	"reflect"
+	"strconv"
 	"strings"
 	"testing"
 )
@@ -820,5 +823,130 @@ func TestJsonPathFunc_Apply1(t *testing.T) {
 			t.Errorf("%d: Invalid returned result found %v", i, result)
 		}
 
+	}
+}
+
+func TestChangedFuncs_Apply1(t *testing.T) {
+	var tests = []struct {
+		sql    string
+		data   []interface{}
+		result [][]map[string]interface{}
+	}{
+		{
+			sql: `SELECT changed_col(true, a), b FROM test`,
+			data: []interface{}{
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"b": "b1",
+						"c": "c1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"b": "b2",
+						"c": "c1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"c": "c1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"b": "b2",
+						"c": "c2",
+					},
+				},
+			},
+			result: [][]map[string]interface{}{{{
+				"changed_col": "a1",
+				"b":           "b1",
+			}}, {{
+				"b": "b2",
+			}}, {{}}, {{
+				"b": "b2",
+			}}},
+		}, {
+			sql: `SELECT changed_col(true, *) FROM test`,
+			data: []interface{}{
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"b": "b1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"c": "c1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"c": "c1",
+					},
+				},
+				&xsql.Tuple{
+					Emitter: "test",
+					Message: xsql.Message{
+						"a": "a1",
+						"b": "b2",
+						"c": "c2",
+					},
+				},
+			},
+			result: [][]map[string]interface{}{{{
+				"changed_col": xsql.Message{
+					"a": "a1",
+					"b": "b1",
+				},
+			}}, {{
+				"changed_col": xsql.Message{
+					"a": "a1",
+					"c": "c1",
+				},
+			}}, {{}}, {{
+				"changed_col": xsql.Message{
+					"a": "a1",
+					"b": "b2",
+					"c": "c2",
+				},
+			}}},
+		},
+	}
+
+	fmt.Printf("The test bucket size is %d.\n\n", len(tests))
+	contextLogger := conf.Log.WithField("rule", "TestChangedFuncs_Apply1")
+
+	for i, tt := range tests[1:] {
+		tempStore, _ := state.CreateStore("mockRule"+strconv.Itoa(i), api.AtMostOnce)
+		ctx := context.WithValue(context.Background(), context.LoggerKey, contextLogger).WithMeta("mockRule"+strconv.Itoa(i), "project", tempStore)
+		stmt, err := xsql.NewParser(strings.NewReader(tt.sql)).Parse()
+		if err != nil || stmt == nil {
+			t.Errorf("parse sql %s error %v", tt.sql, err)
+		}
+		pp := &ProjectOp{Fields: stmt.Fields}
+		fv, afv := xsql.NewFunctionValuersForOp(ctx)
+		r := make([][]map[string]interface{}, 0, len(tt.data))
+		for _, d := range tt.data {
+			result := pp.Apply(ctx, d, fv, afv)
+			r = append(r, result.([]map[string]interface{}))
+		}
+		if !reflect.DeepEqual(tt.result, r) {
+			t.Errorf("%d. %q\n\nresult mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, tt.sql, tt.result, r)
+		}
 	}
 }
