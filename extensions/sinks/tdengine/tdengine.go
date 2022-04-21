6ca29228f5b173aa2f23871fe3108b7f9f2356e7
Jiyong Huang <huangjy@emqx.io>
2022-04-21 16:07:09 +0800

    feat(plugin): tdengine sTable support
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- extensions/sinks/tdengine/tdengine.go
+++ extensions/sinks/tdengine/tdengine.go
@@ -1,4 +1,4 @@
-// Copyright 2021 EMQ Technologies Co., Ltd.
+// Copyright 2021-2022 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,17 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build plugins
-// +build plugins
-
 package main
 
 import (
 	"database/sql"
 	"fmt"
+	"github.com/lf-edge/ekuiper/internal/conf"
 	"github.com/lf-edge/ekuiper/pkg/api"
 	"github.com/lf-edge/ekuiper/pkg/cast"
-	"github.com/lf-edge/ekuiper/pkg/errorx"
 	_ "github.com/taosdata/driver-go/v2/taosSql"
 	"reflect"
 	"strings"
@@ -32,103 +29,142 @@ type (
 	taosConfig struct {
 		ProvideTs   bool     `json:"provideTs"`
 		Port        int      `json:"port"`
-		Ip          string   `json:"ip"`
+		Ip          string   `json:"ip"` // To be deprecated
+		Host        string   `json:"host"`
 		User        string   `json:"user"`
 		Password    string   `json:"password"`
 		Database    string   `json:"database"`
 		Table       string   `json:"table"`
 		TsFieldName string   `json:"tsFieldName"`
 		Fields      []string `json:"fields"`
+		STable      string   `json:"sTable"`
+		TagFields   []string `json:"tagFields"`
 	}
 	taosSink struct {
 		conf *taosConfig
+		url  string
 		db   *sql.DB
 	}
 )
 
-func (this *taosConfig) delTsField() {
+func (t *taosConfig) delTsField() {
 	var auxFields []string
-	for _, v := range this.Fields {
-		if v != this.TsFieldName {
+	for _, v := range t.Fields {
+		if v != t.TsFieldName {
 			auxFields = append(auxFields, v)
 		}
 	}
-	this.Fields = auxFields
+	t.Fields = auxFields
 }
 
-func (this *taosConfig) buildSql(ctx api.StreamContext, mapData map[string]interface{}) (string, error) {
+func (t *taosConfig) buildSql(ctx api.StreamContext, mapData map[string]interface{}) (string, error) {
 	if 0 == len(mapData) {
 		return "", fmt.Errorf("data is empty.")
 	}
-	if 0 == len(this.TsFieldName) {
-		return "", fmt.Errorf("tsFieldName is empty.")
+	logger := ctx.GetLogger()
+	var (
+		table, sTable    string
+		keys, vals, tags []string
+		err              error
+	)
+	table, err = ctx.ParseTemplate(t.Table, mapData)
+	if err != nil {
+		logger.Errorf("parse template for table %s error: %v", t.Table, err)
+		return "", err
+	}
+	sTable, err = ctx.ParseTemplate(t.STable, mapData)
+	if err != nil {
+		logger.Errorf("parse template for sTable %s error: %v", t.STable, err)
+		return "", err
 	}
 
-	logger := ctx.GetLogger()
-	var keys, vals []string
-
-	if this.ProvideTs {
-		if v, ok := mapData[this.TsFieldName]; !ok {
-			return "", fmt.Errorf("Timestamp field not found : %s.", this.TsFieldName)
+	if t.ProvideTs {
+		if v, ok := mapData[t.TsFieldName]; !ok {
+			return "", fmt.Errorf("Timestamp field not found : %s.", t.TsFieldName)
 		} else {
-			keys = append(keys, this.TsFieldName)
+			keys = append(keys, t.TsFieldName)
 			vals = append(vals, fmt.Sprintf(`"%v"`, v))
-			delete(mapData, this.TsFieldName)
-			this.delTsField()
 		}
 	} else {
 		vals = append(vals, "now")
-		keys = append(keys, this.TsFieldName)
+		keys = append(keys, t.TsFieldName)
 	}
 
-	for _, k := range this.Fields {
-		if v, ok := mapData[k]; ok {
+	if len(t.Fields) != 0 {
+		for _, k := range t.Fields {
+			if k == t.TsFieldName {
+				continue
+			}
+			if v, ok := mapData[k]; ok {
+				keys = append(keys, k)
+				if reflect.String == reflect.TypeOf(v).Kind() {
+					vals = append(vals, fmt.Sprintf(`"%v"`, v))
+				} else {
+					vals = append(vals, fmt.Sprintf(`%v`, v))
+				}
+			} else {
+				logger.Warnln("not found field:", k)
+			}
+		}
+	} else {
+		for k, v := range mapData {
+			if k == t.TsFieldName {
+				continue
+			}
 			keys = append(keys, k)
 			if reflect.String == reflect.TypeOf(v).Kind() {
 				vals = append(vals, fmt.Sprintf(`"%v"`, v))
 			} else {
 				vals = append(vals, fmt.Sprintf(`%v`, v))
 			}
-		} else {
-			logger.Warnln("not found field:", k)
 		}
 	}
 
-	if 0 != len(this.Fields) {
-		if len(this.Fields) < len(mapData) {
-			logger.Warnln("some of values will be ignored.")
+	if len(t.TagFields) > 0 {
+		for _, v := range t.TagFields {
+			switch mapData[v].(type) {
+			case string:
+				tags = append(tags, fmt.Sprintf(`"%s"`, mapData[v]))
+			default:
+				tags = append(tags, fmt.Sprintf(`%v`, mapData[v]))
+			}
 		}
-		return fmt.Sprintf(`INSERT INTO %s (%s)VALUES(%s);`, this.Table, strings.Join(keys, `,`), strings.Join(vals, `,`)), nil
 	}
 
-	for k, v := range mapData {
-		keys = append(keys, k)
-		if reflect.String == reflect.TypeOf(v).Kind() {
-			vals = append(vals, fmt.Sprintf(`"%v"`, v))
-		} else {
-			vals = append(vals, fmt.Sprintf(`%v`, v))
-		}
+	sqlStr := fmt.Sprintf("INSERT INTO %s (%s)", table, strings.Join(keys, ","))
+	if sTable != "" {
+		sqlStr += " using " + sTable
 	}
-	if 0 != len(keys) {
-		return fmt.Sprintf(`INSERT INTO %s (%s)VALUES(%s);`, this.Table, strings.Join(keys, `,`), strings.Join(vals, `,`)), nil
+	if len(tags) != 0 {
+		sqlStr += " tags (" + strings.Join(tags, ",") + ")"
 	}
-	return "", nil
+	sqlStr += " values (" + strings.Join(vals, ",") + ");"
+	return sqlStr, nil
 }
 
 func (m *taosSink) Configure(props map[string]interface{}) error {
-	cfg := &taosConfig{}
+	cfg := &taosConfig{
+		User:     "root",
+		Password: "taosdata",
+	}
 	err := cast.MapToStruct(props, cfg)
 	if err != nil {
 		return fmt.Errorf("read properties %v fail with error: %v", props, err)
 	}
-	if cfg.Ip == "" {
-		cfg.Ip = "127.0.0.1"
+	if cfg.Ip != "" {
+		conf.Log.Warnf("Deprecated: Tdengine sink ip property is deprecated, use host instead.")
+		if cfg.Host == "" {
+			cfg.Host = cfg.Ip
+		}
+	}
+	if cfg.Host == "" {
+		cfg.Host = "localhost"
 	}
 	if cfg.User == "" {
-		cfg.User = "root"
+		return fmt.Errorf("propert user is required.")
 	}
 	if cfg.Password == "" {
-		cfg.Password = "taosdata"
+		return fmt.Errorf("propert password is required.")
 	}
 	if cfg.Database == "" {
 		return fmt.Errorf("property database is required")
@@ -139,38 +175,51 @@ func (m *taosSink) Configure(props map[string]interface{}) error {
 	if cfg.TsFieldName == "" {
 		return fmt.Errorf("property TsFieldName is required")
 	}
+	if cfg.STable != "" && len(cfg.TagFields) == 0 {
+		return fmt.Errorf("property tagFields is required when sTable is set")
+	}
+	m.url = fmt.Sprintf(`%s:%s@tcp(%s:%d)/%s`, cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database)
 	m.conf = cfg
 	return nil
 }
 
 func (m *taosSink) Open(ctx api.StreamContext) (err error) {
-	logger := ctx.GetLogger()
-	logger.Debug("Opening tdengine sink")
-	url := fmt.Sprintf(`%s:%s@tcp(%s:%d)/%s`, m.conf.User, m.conf.Password, m.conf.Ip, m.conf.Port, m.conf.Database)
-	m.db, err = sql.Open("taosSql", url)
+	ctx.GetLogger().Debug("Opening tdengine sink")
+	m.db, err = sql.Open("taosSql", m.url)
 	return err
 }
 
 func (m *taosSink) Collect(ctx api.StreamContext, item interface{}) error {
-	logger := ctx.GetLogger()
-	logger.Debugf("tdengine sink receive %s", item)
+	ctx.GetLogger().Debugf("tdengine sink receive %s", item)
 
-	sliData, ok := item.([]map[string]interface{})
-	if !ok {
-		return fmt.Errorf("tdengine sink receive non map slice data: %#v", item)
-	}
-	for _, mapData := range sliData {
-		sql, err := m.conf.buildSql(ctx, mapData)
-		if nil != err {
-			return err
+	switch v := item.(type) {
+	case []map[string]interface{}:
+		var err error
+		for _, mapData := range v {
+			e := m.writeToDB(ctx, mapData)
+			if e != nil {
+				err = e
+			}
 		}
-		logger.Debugf(sql)
-		rows, err := m.db.Query(sql)
-		if err != nil {
-			return fmt.Errorf("%s:%s", errorx.IOErr, err.Error())
-		}
-		rows.Close()
+		return err
+	case map[string]interface{}:
+		return m.writeToDB(ctx, v)
+	default: // never happen
+		return fmt.Errorf("unsupported type: %T", item)
 	}
+}
+
+func (m *taosSink) writeToDB(ctx api.StreamContext, mapData map[string]interface{}) error {
+	sql, err := m.conf.buildSql(ctx, mapData)
+	if nil != err {
+		return err
+	}
+	ctx.GetLogger().Debugf(sql)
+	rows, err := m.db.Query(sql)
+	if err != nil {
+		return err
+	}
+	rows.Close()
 	return nil
 }
 
---
+++ extensions/sinks/tdengine/tdengine_test.go
@@ -0,0 +1,237 @@
+// Copyright 2022 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"github.com/lf-edge/ekuiper/internal/conf"
+	"github.com/lf-edge/ekuiper/internal/testx"
+	"github.com/lf-edge/ekuiper/internal/topo/context"
+	"github.com/lf-edge/ekuiper/internal/topo/state"
+	"reflect"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	var tests = []struct {
+		conf     map[string]interface{}
+		expected *taosConfig
+		error    string
+	}{
+		{ //0
+			conf: map[string]interface{}{
+				"host":        "e0d9d8089bef",
+				"port":        6030,
+				"user":        "root",
+				"password":    "taosdata",
+				"database":    "db",
+				"table":       "t",
+				"tsfieldname": "ts",
+			},
+			expected: &taosConfig{
+				ProvideTs:   false,
+				Host:        "e0d9d8089bef",
+				Port:        6030,
+				User:        "root",
+				Password:    "taosdata",
+				Database:    "db",
+				Table:       "t",
+				TsFieldName: "ts",
+				Fields:      nil,
+			},
+		},
+		{ //1
+			conf: map[string]interface{}{
+				"ip":          "e0d9d8089bef",
+				"port":        6030,
+				"user":        "root1",
+				"password":    "taosdata1",
+				"database":    "db",
+				"table":       "t",
+				"provideTs":   true,
+				"tsfieldname": "ts",
+			},
+			expected: &taosConfig{
+				ProvideTs:   true,
+				Ip:          "e0d9d8089bef",
+				Host:        "e0d9d8089bef",
+				Port:        6030,
+				User:        "root1",
+				Password:    "taosdata1",
+				Database:    "db",
+				Table:       "t",
+				TsFieldName: "ts",
+				Fields:      nil,
+			},
+		},
+		{ //2
+			conf: map[string]interface{}{
+				"port":        6030,
+				"database":    "dab",
+				"table":       "tt",
+				"tsfieldname": "tst",
+				"fields":      []string{"f1", "f2"},
+				"sTable":      "s",
+				"tagFields":   []string{"a", "b"},
+			},
+			expected: &taosConfig{
+				ProvideTs:   false,
+				Ip:          "",
+				Host:        "localhost",
+				Port:        6030,
+				User:        "root",
+				Password:    "taosdata",
+				Database:    "dab",
+				Table:       "tt",
+				TsFieldName: "tst",
+				Fields:      []string{"f1", "f2"},
+				STable:      "s",
+				TagFields:   []string{"a", "b"},
+			},
+		},
+		{ //3
+			conf: map[string]interface{}{
+				"port":     6030,
+				"database": "dab",
+				"table":    "t",
+				"fields":   []string{"f1", "f2"},
+			},
+			error: "property TsFieldName is required",
+		},
+		{ //4
+			conf: map[string]interface{}{
+				"port":        6030,
+				"database":    "dab",
+				"table":       "tt",
+				"tsfieldname": "tst",
+				"fields":      []string{"f1", "f2"},
+				"sTable":      "s",
+			},
+			error: "property tagFields is required when sTable is set",
+		},
+	}
+
+	fmt.Printf("The test bucket size is %d.\n\n", len(tests))
+	for i, test := range tests {
+		tdsink := &taosSink{}
+		err := tdsink.Configure(test.conf)
+		if !reflect.DeepEqual(test.error, testx.Errstring(err)) {
+			t.Errorf("%d: error mismatch:\n  exp=%s\n  got=%s\n\n", i, test.error, err)
+		} else if test.error == "" && !reflect.DeepEqual(test.expected, tdsink.conf) {
+			t.Errorf("%d\n\nresult mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, test.expected, tdsink.conf)
+		}
+	}
+}
+
+func TestBuildSql(t *testing.T) {
+	var tests = []struct {
+		conf     *taosConfig
+		data     map[string]interface{}
+		expected string
+		error    string
+	}{
+		{
+			conf: &taosConfig{
+				ProvideTs:   false,
+				Host:        "e0d9d8089bef",
+				Port:        6030,
+				User:        "root",
+				Password:    "taosdata",
+				Database:    "db",
+				Table:       "t",
+				TsFieldName: "ts",
+				Fields:      nil,
+			},
+			data: map[string]interface{}{
+				"f1": "v1",
+			},
+			expected: `INSERT INTO t (ts,f1) values (now,"v1");`,
+		},
+		{
+			conf: &taosConfig{
+				ProvideTs:   true,
+				Ip:          "e0d9d8089bef",
+				Host:        "e0d9d8089bef",
+				Port:        6030,
+				User:        "root1",
+				Password:    "taosdata1",
+				Database:    "db",
+				Table:       "t",
+				TsFieldName: "ts",
+				Fields:      nil,
+			},
+			data: map[string]interface{}{
+				"ts": 12345678,
+				"f2": 65,
+			},
+			expected: `INSERT INTO t (ts,f2) values ("12345678",65);`,
+		},
+		{
+			conf: &taosConfig{
+				ProvideTs:   true,
+				Ip:          "e0d9d8089bef",
+				Host:        "e0d9d8089bef",
+				Port:        6030,
+				User:        "root1",
+				Password:    "taosdata1",
+				Database:    "db",
+				Table:       "t",
+				TsFieldName: "ts",
+				Fields:      nil,
+			},
+			data: map[string]interface{}{
+				"ts": 12345678,
+				"f2": 65,
+			},
+			expected: `INSERT INTO t (ts,f2) values ("12345678",65);`,
+		},
+		{
+			conf: &taosConfig{
+				ProvideTs:   false,
+				Ip:          "",
+				Host:        "localhost",
+				Port:        6030,
+				User:        "root",
+				Password:    "taosdata",
+				Database:    "dab",
+				Table:       "{{.table}}",
+				TsFieldName: "tst",
+				Fields:      []string{"f1", "f2"},
+				STable:      "s",
+				TagFields:   []string{"a", "b"},
+			},
+			data: map[string]interface{}{
+				"table": "t1",
+				"ts":    12345678,
+				"f2":    65,
+				"f1":    12.3,
+				"a":     "a1",
+				"b":     2,
+			},
+			expected: `INSERT INTO t1 (tst,f1,f2) using s tags ("a1",2) values (now,12.3,65);`,
+		},
+	}
+	fmt.Printf("The test bucket size is %d.\n\n", len(tests))
+	contextLogger := conf.Log.WithField("rule", "mockRule0")
+	ctx := context.WithValue(context.Background(), context.LoggerKey, contextLogger).WithMeta("testTD", "op1", &state.MemoryStore{})
+	for i, test := range tests {
+		sql, err := test.conf.buildSql(ctx, test.data)
+		if !reflect.DeepEqual(test.error, testx.Errstring(err)) {
+			t.Errorf("%d: error mismatch:\n  exp=%s\n  got=%s\n\n", i, test.error, err)
+		} else if test.error == "" && !reflect.DeepEqual(test.expected, sql) {
+			t.Errorf("%d\n\nresult mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, test.expected, sql)
+		}
+	}
+}
