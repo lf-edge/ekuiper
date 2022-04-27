8c237aaeabc0c67cf54db9119065e4490a60905d
Jiyong Huang <huangjy@emqx.io>
2022-04-27 09:45:03 +0800

    test(benchmark): benchmark test for multiple rules
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ test/benchmark/multiple_rules/pub.go
@@ -0,0 +1,42 @@
+// Copyright 2021-2022 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"time"
+)
+
+const (
+	tps = 500
+)
+
+func pub() {
+	opts := mqtt.NewClientOptions().AddBroker(mqttUrl)
+	client := mqtt.NewClient(opts)
+	defer client.Disconnect(0)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	for i := 0; ; i++ {
+		payload := []byte(fmt.Sprintf(`{"temperature":%d,"humidity":90}`, 12+i%10))
+		if token := client.Publish("rawdata", 0, false, payload); token.Wait() && token.Error() != nil {
+			fmt.Println(token.Error())
+		}
+
+		time.Sleep(1000 / tps * time.Millisecond)
+	}
+}
