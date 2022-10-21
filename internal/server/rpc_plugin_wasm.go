2e373b5228c0945cd56a05f667cebe5d26a60e82
erfenjiao <630166475@qq.com>
2022-10-21 11:59:16 +0800

    10-21-2
    Signed-off-by: erfenjiao <630166475@qq.com>

--- internal/server/rpc_plugin_wasm.go
+++ internal/server/rpc_plugin_wasm.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build wasm && rpc && core && !plugin
-// +build wasm,rpc,core,!plugin
+//go:build wasm
+// +build wasm
 
 package server
 
--- internal/server/wasm_init.go
+++ internal/server/wasm_init.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build wasm || !core
-// +build wasm !core
+//go:build wasm
+// +build wasm
 
 package server
 
