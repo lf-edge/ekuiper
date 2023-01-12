685169bbfb64f2892d8b3ea1bef2d15ad7c51afc
Jiyong Huang <huangjy@emqx.io>
2023-01-12 14:39:50 +0800

    test(service): add unit test for service manager
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/service/manager_test.go
+++ internal/service/manager_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 EMQ Technologies Co., Ltd.
+// Copyright 2021-2023 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,9 +15,16 @@
 package service
 
 import (
+	"archive/zip"
+	"fmt"
 	"github.com/lf-edge/ekuiper/internal/binder"
 	"github.com/lf-edge/ekuiper/internal/binder/function"
+	"net/url"
+	"os"
+	"path/filepath"
 	"reflect"
+	"sort"
+	"strings"
 	"testing"
 )
 
@@ -299,4 +306,230 @@ func TestInitByFiles(t *testing.T) {
 			t.Errorf("func info mismatch, expect %v but got %v", c, actualFunc)
 		}
 	}
+}
+
+func TestManage(t *testing.T) {
+	// Test HasFunctionSet
+	if m.HasFunctionSet("sample") {
+		t.Error("HasFunctionSet failed, got true should be false")
+	}
+	if !m.HasService("sample") {
+		t.Error("service sample not found")
+	}
+
+	_, err := m.Function("ListShelves")
+	if err != nil {
+		t.Errorf("Function ListShelves failed: %v", err)
+	}
+
+	_, ok := m.ConvName("ListShelves")
+	if !ok {
+		t.Error("ConvName for ListShelves failed")
+	}
+
+	_, ok = m.ConvName("NotExist")
+	if ok {
+		t.Error("ConvName for NotExist should failed")
+	}
+
+	initServices := []string{"httpSample", "sample"}
+	list, err := m.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(initServices, list) {
+		t.Errorf("Get service list error, \nexpect\t\t%v, \nbut got\t\t%v", initServices, list)
+	}
+	// Create the zip file
+	baseFolder := filepath.Join(m.etcDir, "toadd")
+	os.MkdirAll(filepath.Join(m.etcDir, "temp"), 0755)
+	outPath := filepath.Join(m.etcDir, "temp", "dynamic.zip")
+	outFile, err := os.Create(outPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer os.Remove(outPath)
+
+	// Create a new zip archive.
+	w := zip.NewWriter(outFile)
+	addFiles(w, baseFolder, "")
+	err = w.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Install the dynamic zip
+	url, err := urlFromFilePath(outPath)
+	err = m.Create(&ServiceCreationRequest{
+		Name: "dynamic",
+		File: url.String(),
+	})
+	if err != nil {
+		t.Errorf("Create dynamic service failed: %v", err)
+		return
+	}
+	dService, err := m.Get("dynamic")
+	if err != nil {
+		t.Errorf("Get dynamic service error: %v", err)
+	} else if len(dService.Interfaces) != 1 {
+		t.Errorf("dynamic service should have 1 interface, but got %d", len(dService.Interfaces))
+	}
+
+	expectedService := map[string]string{
+		"dynamic": `{"name":"dynamic","file":"` + url.String() + `"}`,
+	}
+	allServices := m.GetAllServices()
+	if !reflect.DeepEqual(expectedService, allServices) {
+		t.Errorf("Get all installed service faile \nexpect\t\t%v, \nbut got\t\t%v", expectedService, allServices)
+	}
+
+	allServicesStatus := m.GetAllServicesStatus()
+	if len(allServicesStatus) != 0 {
+		t.Errorf("Get all installed service status faile, expect 0 but got %d", len(allServicesStatus))
+	}
+
+	expectedFunctions := []string{"ListShelves", "CreateShelf", "GetShelf", "DeleteShelf", "ListBooks", "createBook", "GetBook", "DeleteBook", "GetMessage", "SearchMessage", "UpdateMessage", "PatchMessage", "helloFromGrpc", "ComputeFromGrpc", "getFeatureFromGrpc", "objectDetectFromGrpc", "getStatusFromGrpc", "notUsedRpc", "helloFromRest", "ComputeFromRest", "getFeatureFromRest", "objectDetectFromRest", "getStatusFromRest", "restEncodedJson", "helloFromMsgpack", "ComputeFromMsgpack", "getFeatureFromMsgpack", "objectDetectFromMsgpack", "getStatusFromMsgpack", "notUsedMsgpack", "SayHello2"}
+	sort.Strings(expectedFunctions)
+
+	functions, err := m.ListFunctions()
+	sort.Strings(functions)
+	if !reflect.DeepEqual(expectedFunctions, functions) {
+		t.Errorf("Get all installed functions faile \nexpect\t\t%v, \nbut got\t\t%v", expectedFunctions, functions)
+	}
+
+	err = m.Update(&ServiceCreationRequest{
+		Name: "dynamic",
+		File: url.String(),
+	})
+	if err != nil {
+		t.Errorf("Create dynamic service failed: %v", err)
+		return
+	}
+
+	m.UninstallAllServices()
+	allServices = m.GetAllServices()
+	if len(allServices) != 0 {
+		t.Errorf("Uninstall all services failed, expect 0 but got %d", len(allServices))
+	}
+
+	importedService := map[string]string{
+		"wrongFormat": "nnn",
+		"dynamic":     `{"name":"dynamic","file":"` + url.String() + `"}`,
+		"wrongPath":   `{"name":"dynamic","file":"wrongpath"}`,
+	}
+	m.ImportServices(importedService)
+
+	allServicesStatus = m.GetAllServicesStatus()
+	if len(allServicesStatus) != 2 {
+		t.Errorf("Get all installed service status faile, expect 2 error but got %v", allServicesStatus)
+	}
+
+	expectedList := []string{"httpSample", "sample", "dynamic"}
+	list, err = m.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(expectedList, list) {
+		t.Errorf("Get service list error, \nexpect\t\t%v, \nbut got\t\t%v", expectedList, list)
+	}
+
+	err = m.Delete("dynamic")
+	if err != nil {
+		t.Errorf("Delete dynamic service error: %v", err)
+	}
+
+	list, err = m.List()
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(initServices, list) {
+		t.Errorf("Get service list error, \nexpect\t\t%v, \nbut got\t\t%v", initServices, list)
+	}
+}
+
+func addFiles(w *zip.Writer, basePath, baseInZip string) {
+	// Open the Directory
+	files, err := os.ReadDir(basePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			dat, err := os.ReadFile(filepath.Join(basePath, file.Name()))
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			// Add some files to the archive.
+			f, err := w.Create(filepath.Join(baseInZip, file.Name()))
+			if err != nil {
+				fmt.Println(err)
+			}
+			_, err = f.Write(dat)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if file.IsDir() {
+
+			// Recurse
+			newBase := filepath.Join(basePath, file.Name())
+			fmt.Println("Recursing and Adding SubDir: " + file.Name())
+			fmt.Println("Recursing and Adding SubDir: " + newBase)
+
+			addFiles(w, newBase, filepath.Join(baseInZip, file.Name()))
+		}
+	}
+}
+
+func urlFromFilePath(path string) (*url.URL, error) {
+	if !filepath.IsAbs(path) {
+		return nil, fmt.Errorf("path %s is not absolute", path)
+	}
+
+	// If path has a Windows volume name, convert the volume to a host and prefix
+	// per https://blogs.msdn.microsoft.com/ie/2006/12/06/file-uris-in-windows/.
+	if vol := filepath.VolumeName(path); vol != "" {
+		if strings.HasPrefix(vol, `\\`) {
+			path = filepath.ToSlash(path[2:])
+			i := strings.IndexByte(path, '/')
+
+			if i < 0 {
+				// A degenerate case.
+				// \\host.example.com (without a share name)
+				// becomes
+				// file://host.example.com/
+				return &url.URL{
+					Scheme: "file",
+					Host:   path,
+					Path:   "/",
+				}, nil
+			}
+
+			// \\host.example.com\Share\path\to\file
+			// becomes
+			// file://host.example.com/Share/path/to/file
+			return &url.URL{
+				Scheme: "file",
+				Host:   path[:i],
+				Path:   filepath.ToSlash(path[i:]),
+			}, nil
+		}
+
+		// C:\path\to\file
+		// becomes
+		// file:///C:/path/to/file
+		return &url.URL{
+			Scheme: "file",
+			Path:   "/" + filepath.ToSlash(path),
+		}, nil
+	}
+
+	// /path/to/file
+	// becomes
+	// file:///path/to/file
+	return &url.URL{
+		Scheme: "file",
+		Path:   filepath.ToSlash(path),
+	}, nil
 }
