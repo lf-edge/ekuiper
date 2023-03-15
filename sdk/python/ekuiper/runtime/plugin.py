80a4c51ca18ad3b0578d91813d16c31beaa792a4
Jiyong Huang <huangjy@emqx.io>
2023-03-15 20:49:58 +0800

    fix(portable): remedy hot reload and full import
    - revert the previous fix to adapt to import portable data
    - clarify function instance management so that the instance is reusable
    - add more log
    
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- sdk/python/ekuiper/runtime/plugin.py
+++ sdk/python/ekuiper/runtime/plugin.py
@@ -1,4 +1,4 @@
-#  Copyright 2021 EMQ Technologies Co., Ltd.
+#  Copyright 2021-2023 EMQ Technologies Co., Ltd.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 import json
 import logging
 import sys
@@ -59,6 +60,7 @@ def start(c: PluginConfig):
     logging.info("starting plugin {}".format(c.name))
     ch = PairChannel(c.name, 0)
     ch.run(command_reply)
+    logging.info("started plugin {}".format(c.name))
 
 
 def init_vars(c: PluginConfig):
