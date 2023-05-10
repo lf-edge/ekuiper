b53036c8787eded689e485644bb945b6ee686f11
Jianxiang Ran <rxan_embedded@163.com>
2023-05-10 13:33:44 +0800

    fix(wasm): fix core lint issue
    Signed-off-by: Jianxiang Ran <rxan_embedded@163.com>

--- internal/server/core.go
+++ internal/server/core.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (core && !plugin && !portable && !service && !schema)
+//go:build core && !plugin && !portable && !service && !schema
 
 package server
 
