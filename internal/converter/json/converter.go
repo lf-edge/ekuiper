925da0330b5047b4ec420371f7a93d49f919490d
yisaer <disxiaofei@163.com>
2023-05-25 15:38:35 +0800

    introduce fastjson
    Signed-off-by: yisaer <disxiaofei@163.com>

--- internal/converter/json/converter.go
+++ internal/converter/json/converter.go
@@ -16,7 +16,11 @@ package json
 
 import (
 	"encoding/json"
+	"fmt"
 
+	"github.com/valyala/fastjson"
+
+	"github.com/lf-edge/ekuiper/pkg/ast"
 	"github.com/lf-edge/ekuiper/pkg/message"
 )
 
@@ -39,4 +43,148 @@ func (c *Converter) Decode(b []byte) (interface{}, error) {
 		return nil, err
 	}
 	return r0, nil
+}
+
+type FastJsonConverter struct{}
+
+var fastConverter = &FastJsonConverter{}
+
+func (f *FastJsonConverter) DecodeWithSchema(b []byte, schema map[string]*ast.JsonStreamField) (interface{}, error) {
+	var p fastjson.Parser
+	v, err := p.ParseBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	switch v.Type() {
+	case fastjson.TypeArray:
+		array, err := v.Array()
+		if err != nil {
+			return nil, err
+		}
+		ms := make([]map[string]interface{}, len(array))
+		for i, v := range array {
+			obj, err := v.Object()
+			if err != nil {
+				return nil, err
+			}
+			subMap, err := f.decodeObject(obj, schema)
+			ms[i] = subMap
+		}
+		return ms, nil
+	case fastjson.TypeObject:
+		obj, err := v.Object()
+		if err != nil {
+			return nil, err
+		}
+		m, err := f.decodeObject(obj, schema)
+		return m, nil
+	}
+	return nil, fmt.Errorf("err2")
+}
+
+func (f *FastJsonConverter) decodeArray(array []*fastjson.Value, field *ast.JsonStreamField) ([]interface{}, error) {
+	vs := make([]interface{}, len(array))
+	switch field.Type {
+	case "bigint", "float", "datetime":
+		for i, item := range array {
+			f64, err := item.Float64()
+			if err != nil {
+				return nil, err
+			}
+			vs[i] = f64
+		}
+	case "string", "bytea":
+		for i, item := range array {
+			s, err := item.StringBytes()
+			if err != nil {
+				return nil, err
+			}
+			vs[i] = string(s)
+		}
+	case "array":
+		for i, item := range array {
+			childArrays, err := item.Array()
+			if err != nil {
+				return nil, err
+			}
+			subList, err := f.decodeArray(childArrays, field.Items)
+			if err != nil {
+				return nil, err
+			}
+			vs[i] = subList
+		}
+	case "struct":
+		for i, item := range array {
+			childObj, err := item.Object()
+			if err != nil {
+				return nil, err
+			}
+			subMap, err := f.decodeObject(childObj, field.Properties)
+			if err != nil {
+				return nil, err
+			}
+			vs[i] = subMap
+		}
+	case "boolean":
+		for i, item := range array {
+			b, err := item.Bool()
+			if err != nil {
+				return nil, err
+			}
+			vs[i] = b
+		}
+	default:
+		return nil, fmt.Errorf("decodeArray err")
+	}
+	return vs, nil
+}
+
+func (f *FastJsonConverter) decodeObject(obj *fastjson.Object, schema map[string]*ast.JsonStreamField) (map[string]interface{}, error) {
+	m := make(map[string]interface{})
+	for key, field := range schema {
+		switch field.Type {
+		case "bigint", "float", "datetime":
+			f64v, err := obj.Get(key).Float64()
+			if err != nil {
+				return nil, err
+			}
+			m[key] = f64v
+		case "string", "bytea":
+			s, err := obj.Get(key).StringBytes()
+			if err != nil {
+				return nil, err
+			}
+			m[key] = string(s)
+		case "array":
+			childArray, err := obj.Get(key).Array()
+			if err != nil {
+				return nil, err
+			}
+			subList, err := f.decodeArray(childArray, schema[key].Items)
+			if err != nil {
+				return nil, err
+			}
+			m[key] = subList
+		case "struct":
+			childObj, err := obj.Get(key).Object()
+			if err != nil {
+				return nil, err
+			}
+			childMap, err := f.decodeObject(childObj, schema[key].Properties)
+			if err != nil {
+				return nil, err
+			}
+			m[key] = childMap
+		case "boolean":
+			b, err := obj.Get(key).Bool()
+			if err != nil {
+				return nil, err
+			}
+			m[key] = b
+		default:
+			return nil, fmt.Errorf("decodeObject err")
+		}
+	}
+
+	return m, nil
 }
