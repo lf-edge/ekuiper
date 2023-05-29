c7cfdf711279ff1a261243feb54fa46471db9253
yisaer <disxiaofei@163.com>
2023-05-29 15:07:40 +0800

    add log
    Signed-off-by: yisaer <disxiaofei@163.com>

--- internal/converter/converter.go
+++ internal/converter/converter.go
@@ -47,9 +47,9 @@ func GetOrCreateConverter(options *ast.Options) (message.Converter, error) {
 	if t == "" {
 		t = message.FormatJson
 	}
-	if t == message.FormatJson && len(options.Schema) > 0 {
-		return json.NewFastJsonConverter(options.Schema), nil
-	}
+	//if t == message.FormatJson && len(options.Schema) > 0 {
+	//	return json.NewFastJsonConverter(options.Schema), nil
+	//}
 
 	schemaFile := ""
 	schemaName := options.SCHEMAID
