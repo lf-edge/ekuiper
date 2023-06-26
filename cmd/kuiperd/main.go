eba3c4d579cbe224d390727108e5143bb93166c0
xjasonlyu <xjasonlyu@gmail.com>
2023-06-26 10:20:20 +0800

    feat: honor cpu limits in cgroups
    Signed-off-by: xjasonlyu <xjasonlyu@gmail.com>

--- cmd/kuiperd/main.go
+++ cmd/kuiperd/main.go
@@ -1,4 +1,4 @@
-// Copyright 2021 EMQ Technologies Co., Ltd.
+// Copyright 2021-2023 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,11 @@
 
 package main
 
-import "github.com/lf-edge/ekuiper/internal/server"
+import (
+	_ "go.uber.org/automaxprocs"
+
+	"github.com/lf-edge/ekuiper/internal/server"
+)
 
 var (
 	Version      = "unknown"
