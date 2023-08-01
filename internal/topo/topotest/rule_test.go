69bebf31e973bb97161010858163aa74fedc3e1a
xjasonlyu <xjasonlyu@gmail.com>
2023-08-01 10:35:58 +0800

    test: add rules test options
    Signed-off-by: xjasonlyu <xjasonlyu@gmail.com>

--- internal/topo/topotest/rule_test.go
+++ internal/topo/topotest/rule_test.go
@@ -329,6 +329,12 @@ func TestLimitSQL(t *testing.T) {
 			SendError:    true,
 		},
 		{
+			BufferLength: 100,
+			SendError:    true,
+			Debug:        true,
+			LogPath:      "rule-test.log",
+		},
+		{
 			BufferLength:       100,
 			SendError:          true,
 			Qos:                api.AtLeastOnce,
