676516f4c5c48e74f5210a43a40c49087e7f53cd
yisaer <disxiaofei@163.com>
2023-08-03 17:48:31 +0800

    add test
    Signed-off-by: yisaer <disxiaofei@163.com>

--- pkg/schedule/schedule_test.go
+++ pkg/schedule/schedule_test.go
@@ -31,3 +31,40 @@ func TestIsRuleInRunningSchedule(t *testing.T) {
 	require.True(t, isInSchedule)
 	require.Equal(t, remainedDuration, time.Second)
 }
+
+func TestIsInScheduleRange(t *testing.T) {
+	now, err := time.Parse(layout, "2006-01-02 15:04:01")
+	require.NoError(t, err)
+	_, err = IsInScheduleRange(now, "", "")
+	require.Error(t, err)
+	_, err = IsInScheduleRange(now, "2006-01-02 15:04:01", "")
+	require.Error(t, err)
+	isIn, err := IsInScheduleRange(now, "2006-01-02 15:04:00", "2006-01-02 15:04:03")
+	require.True(t, isIn)
+	isIn, err = IsInScheduleRange(now, "2006-01-02 15:05:00", "2006-01-02 15:05:03")
+	require.False(t, isIn)
+}
+
+func TestIsAfterTimeRange(t *testing.T) {
+	now, err := time.Parse(layout, "2006-01-02 15:04:01")
+	require.NoError(t, err)
+	_, err = IsAfterTimeRange(now, "")
+	require.Error(t, err)
+	isAfter, err := IsAfterTimeRange(now, "2006-01-02 15:04:00")
+	require.True(t, isAfter)
+	isAfter, err = IsAfterTimeRange(now, "2006-01-02 15:04:00")
+	require.False(t, isAfter)
+}
+
+func TestIsInRunningSchedule(t *testing.T) {
+	now, err := time.Parse(layout, "2006-01-02 15:04:02")
+	require.NoError(t, err)
+	_, _, err = IsInRunningSchedule("", now, time.Second)
+	require.Error(t, err)
+	isIn, _, err := IsInRunningSchedule("4 15 * * *", now, 3*time.Second)
+	require.NoError(t, err)
+	require.True(t, isIn)
+	isIn, _, err = IsInRunningSchedule("4 15 * * *", now, time.Second)
+	require.NoError(t, err)
+	require.False(t, isIn)
+}
