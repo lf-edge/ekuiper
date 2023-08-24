c74f3e8a1b41f2c0f03822f138fb61b9e83df2ea
yisaer <disxiaofei@163.com>
2023-08-24 16:45:08 +0800

    fix test
    Signed-off-by: yisaer <disxiaofei@163.com>

--- internal/server/rest_test.go
+++ internal/server/rest_test.go
@@ -454,7 +454,7 @@ func (suite *RestTestSuite) Test_dataImport() {
 	w2 := httptest.NewRecorder()
 	suite.r.ServeHTTP(w2, req2)
 
-	req, _ = http.NewRequest(http.MethodGet, "http://localhost:8080/data/export?group=any", bytes.NewBufferString("any"))
+	req, _ = http.NewRequest(http.MethodGet, "http://localhost:8080/data/export?group=group", bytes.NewBufferString("any"))
 	w = httptest.NewRecorder()
 	suite.r.ServeHTTP(w, req)
 	assert.Equal(suite.T(), http.StatusOK, w.Code)
