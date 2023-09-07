c5167e8d6225bc09b5ddf50fe692d3d9395b6453
Ming <poxiaobbs@gmail.com>
2023-09-07 15:00:47 +0800

    chore(ci): support multiple versions of documents (#2238)
    Signed-off-by: Swilder-M <poxiaobbs@gmail.com>

--- .github/scripts/generate_version.py
+++ .github/scripts/generate_version.py
@@ -2,8 +2,14 @@ import sys
 import json
 
 version_list = sys.argv[1:]
+if len(version_list) == 1:
+    version_list = version_list[0].split(' ')
+
 
 if __name__ == '__main__':
+    version_list = ['.'.join(version.split('.')[:2]) for version in version_list]
+    version_list = [version for version in version_list if int(version.split('.')[0]) >= 1 and int(version.split('.')[1]) >= 11]
+    version_list = list(set(version_list))
     version_list.sort(
         key=lambda v: [int(u) for u in v.split('.')],
         reverse=True
