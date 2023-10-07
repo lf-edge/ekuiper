3a44e770c484e0b8e8763839e76ef5af053329bc
yisaer <disxiaofei@163.com>
2023-10-07 14:45:07 +0800

    add test
    Signed-off-by: yisaer <disxiaofei@163.com>

--- internal/server/rest_test.go
+++ internal/server/rest_test.go
@@ -61,6 +61,7 @@ func (suite *RestTestSuite) SetupTest() {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", rootHandler).Methods(http.MethodGet, http.MethodPost)
+	r.HandleFunc("/system/status", getMetricsHandler).Methods(http.MethodGet)
 	r.HandleFunc("/ping", pingHandler).Methods(http.MethodGet)
 	r.HandleFunc("/streams", streamsHandler).Methods(http.MethodGet, http.MethodPost)
 	r.HandleFunc("/streams/{name}", streamHandler).Methods(http.MethodGet, http.MethodDelete, http.MethodPut)
@@ -598,4 +599,12 @@ func (suite *ServerTestSuite) TestStartRuleAfterSchemaChange() {
 	err = suite.s.StartRule(ruleId, &reply)
 	assert.Error(suite.T(), err)
 	assert.Equal(suite.T(), err.Error(), "unknown field a")
+}
+
+func (suite *RestTestSuite) Test_metrcisHandler() {
+	// get scan table
+	req, _ := http.NewRequest(http.MethodGet, "http://localhost:8080/streams?kind=scan", bytes.NewBufferString("any"))
+	w := httptest.NewRecorder()
+	suite.r.ServeHTTP(w, req)
+	assert.Equal(suite.T(), http.StatusOK, w.Code)
 }
