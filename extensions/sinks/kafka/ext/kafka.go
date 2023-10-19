48c838b9a2e01d44b458397191e569fd3c82da5d
yisaer <disxiaofei@163.com>
2023-10-19 14:50:22 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

--- extensions/sinks/kafka/ext/kafka.go
+++ extensions/sinks/kafka/ext/kafka.go
@@ -238,12 +238,15 @@ func (m *kafkaSink) Collect(ctx api.StreamContext, item interface{}) error {
 	case kafkago.WriteErrors:
 		count := 0
 		for i := range messages {
+			if strings.Contains(err[i].Error(), "kafka.(*Client).Produce:") {
+				return fmt.Errorf(`%s: kafka sink fails to send out the data . %v`, errorx.IOErr, err)
+			}
 			switch err := err[i].(type) {
 			case nil:
 				continue
 
 			case kafkago.Error:
-				if err.Temporary() || strings.Contains(err.Error(), "kafka.(*Client).Produce:") {
+				if err.Temporary() {
 					count++
 					continue
 				}
