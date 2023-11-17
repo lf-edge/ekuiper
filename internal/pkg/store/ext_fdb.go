c363c1887a11935bdb7d25c6c1d7ba1818411dbf
Jiyong Huang <huangjy@emqx.io>
2023-11-17 08:53:53 +0800

    chore: disable fdb for arm temporally
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/pkg/store/ext_fdb.go
+++ internal/pkg/store/ext_fdb.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build fdb || full
+//go:build amd64 && (fdb || full)
 
 package store
 
