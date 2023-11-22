a556e938ee57e291fbd00984285cb94c42c2ee90
Jiyong Huang <huangjy@emqx.io>
2023-11-22 14:44:02 +0800

    fix(jwt): support []string type aud
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/server/middleware/auth.go
+++ internal/server/middleware/auth.go
@@ -1,3 +1,17 @@
+// Copyright 2023 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package middleware
 
 import (
@@ -30,8 +44,15 @@ var Auth = func(next http.Handler) http.Handler {
 			http.Error(w, err.Error(), http.StatusUnauthorized)
 			return
 		}
-		if tk.StandardClaims.Audience != "eKuiper" {
-			http.Error(w, fmt.Sprintf("audience field should be eKuiper, but got %s", tk.StandardClaims.Audience), http.StatusUnauthorized)
+		hit := false
+		for _, value := range tk.RegisteredClaims.Audience {
+			if value == "eKuiper" {
+				hit = true
+				break
+			}
+		}
+		if !hit {
+			http.Error(w, fmt.Sprintf("audience field should contain eKuiper, but got %s", tk.RegisteredClaims.Audience), http.StatusUnauthorized)
 			return
 		}
 
