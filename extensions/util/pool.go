c6940f08717c606bce57df98a4562212c89ce16e
Song Gao <disxiaofei@163.com>
2024-01-09 11:41:52 +0800

    fix: support hide url password in log (#2542)
    Signed-off-by: yisaer <disxiaofei@163.com>

--- extensions/util/pool.go
+++ extensions/util/pool.go
@@ -22,6 +22,7 @@ import (
 	"github.com/xo/dburl"
 
 	"github.com/lf-edge/ekuiper/internal/conf"
+	"github.com/lf-edge/ekuiper/pkg/hidden"
 )
 
 var GlobalPool *dbPool
@@ -61,14 +62,15 @@ func (dp *dbPool) getDBConnCount(url string) int {
 }
 
 func (dp *dbPool) createAndReplace(url string) (*sql.DB, error) {
+	hiddenURL, _ := hidden.HiddenURLPasswd(url)
 	dp.Lock()
 	defer dp.Unlock()
 	newDb, err := openDB(url, dp.isTesting)
 	if err != nil {
-		conf.Log.Errorf("create new database instance %v failed, err:%v", url, err)
+		conf.Log.Errorf("create new database instance %v failed, err:%v", hiddenURL, err)
 		return nil, err
 	}
-	conf.Log.Infof("create new database instance: %v", url)
+	conf.Log.Infof("create new database instance: %v", hiddenURL)
 	oldDB, ok := dp.pool[url]
 	if !ok {
 		dp.connections[url] = 1
@@ -81,6 +83,7 @@ func (dp *dbPool) createAndReplace(url string) (*sql.DB, error) {
 }
 
 func (dp *dbPool) getOrCreate(url string) (*sql.DB, error) {
+	hiddenURL, _ := hidden.HiddenURLPasswd(url)
 	dp.Lock()
 	defer dp.Unlock()
 	db, ok := dp.pool[url]
@@ -90,10 +93,10 @@ func (dp *dbPool) getOrCreate(url string) (*sql.DB, error) {
 	}
 	newDb, err := openDB(url, dp.isTesting)
 	if err != nil {
-		conf.Log.Errorf("create new database instance %v failed, err:%v", url, err)
+		conf.Log.Errorf("create new database instance %v failed, err:%v", hiddenURL, err)
 		return nil, err
 	}
-	conf.Log.Infof("create new database instance: %v", url)
+	conf.Log.Infof("create new database instance: %v", hiddenURL)
 	dp.pool[url] = newDb
 	dp.connections[url] = 1
 	return newDb, nil
@@ -134,6 +137,7 @@ func openDMDB(url string) (*sql.DB, error) {
 }
 
 func (dp *dbPool) closeOneConn(url string) error {
+	hiddenURL, _ := hidden.HiddenURLPasswd(url)
 	dp.Lock()
 	defer dp.Unlock()
 	connCount, ok := dp.connections[url]
@@ -145,7 +149,7 @@ func (dp *dbPool) closeOneConn(url string) error {
 		dp.connections[url] = connCount
 		return nil
 	}
-	conf.Log.Infof("drop database instance: %v", url)
+	conf.Log.Infof("drop database instance: %v", hiddenURL)
 	db := dp.pool[url]
 	// remove db instance from map in order to avoid memory leak
 	delete(dp.pool, url)
