238685c6eb53c1973104d6f0d2c699bf3a9ebb05
carlclone <906561974@qq.com>
2024-01-22 16:31:14 +0800

    test: add more test for lines format (#2570)
    Signed-off-by: carlclone <906561974@qq.com>

--- internal/io/mock/test_source.go
+++ internal/io/mock/test_source.go
@@ -16,6 +16,7 @@ package mock
 
 import (
 	"fmt"
+	"reflect"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -25,6 +26,7 @@ import (
 	"github.com/lf-edge/ekuiper/internal/converter"
 	mockContext "github.com/lf-edge/ekuiper/internal/io/mock/context"
 	"github.com/lf-edge/ekuiper/internal/topo/context"
+	"github.com/lf-edge/ekuiper/internal/xsql"
 	"github.com/lf-edge/ekuiper/pkg/api"
 	"github.com/lf-edge/ekuiper/pkg/ast"
 )
@@ -41,6 +43,9 @@ func TestSourceOpen(r api.Source, exp []api.SourceTuple, t *testing.T) {
 		case *api.DefaultSourceTuple:
 			assert.Equal(t, exp[i].Message(), v.Message())
 			assert.Equal(t, exp[i].Meta(), v.Meta())
+		case *xsql.ErrorSourceTuple:
+			assert.Equal(t, reflect.TypeOf(exp[i]), reflect.TypeOf(v))
+			assert.Equal(t, exp[i].(*xsql.ErrorSourceTuple).Error, v.(*xsql.ErrorSourceTuple).Error)
 		default:
 			assert.Equal(t, exp[i], v)
 		}
