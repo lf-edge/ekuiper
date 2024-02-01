f9dd94c3da0a1e24aca6e295e10c4941ed32188f
ngjaying <ngjaying@gmail.com>
2024-02-01 14:40:35 +0800

    feat(function): object pick enhancement (#232)
    - Support nested pick a.b
    - Support multiple string args
    
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/binder/function/funcs_obj.go
+++ internal/binder/function/funcs_obj.go
@@ -1,4 +1,4 @@
-// Copyright 2023 EMQ Technologies Co., Ltd.
+// Copyright 2023-2024 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package function
 import (
 	"fmt"
 	"reflect"
+	"strings"
 
 	"github.com/lf-edge/ekuiper/pkg/api"
 	"github.com/lf-edge/ekuiper/pkg/ast"
@@ -235,49 +236,62 @@ func registerObjectFunc() {
 		},
 		check: returnNilIfHasAnyNil,
 	}
+	builtins["object_size"] = builtinFunc{
+		fType: ast.FuncTypeScalar,
+		exec: func(ctx api.FunctionContext, args []interface{}) (interface{}, bool) {
+			if args[0] == nil {
+				return 0, true
+			}
+			argMap, ok := args[0].(map[string]any)
+			if !ok {
+				return fmt.Errorf("the first argument should be map[string]any, got %v", args[0]), false
+			}
+			return len(argMap), true
+		},
+		val: func(ctx api.FunctionContext, args []ast.Expr) error {
+			return ValidateOneArg(ctx, args)
+		},
+	}
+	// The argument can be {obj}, {string arr} OR {obj}, {string}...
 	builtins["object_pick"] = builtinFunc{
 		fType: ast.FuncTypeScalar,
 		exec: func(ctx api.FunctionContext, args []interface{}) (interface{}, bool) {
-			if len(args) != 2 {
-				return fmt.Errorf("the argument number should be 2, got %v", len(args)), false
+			if args[0] == nil {
+				return nil, true
 			}
 			res := make(map[string]interface{})
-			argMap, ok := args[0].(map[string]interface{})
+			argMap, ok := args[0].(map[string]any)
 			if !ok {
-				return fmt.Errorf("the first argument should be map[string]interface{}, got %v", args[0]), false
+				return fmt.Errorf("the first argument should be a map, got %v", args[0]), false
 			}
-			pickArray := make([]string, 0)
-			v := reflect.ValueOf(args[1])
-			switch v.Kind() {
-			case reflect.Slice:
-				array, err := cast.ToStringSlice(args[1], cast.CONVERT_ALL)
-				if err != nil {
-					return err, false
-				}
-				pickArray = append(pickArray, array...)
-			case reflect.String:
-				str := args[1].(string)
-				for k, v := range argMap {
-					if k == str {
-						res[k] = v
+			switch at := args[1].(type) {
+			case []interface{}:
+				for _, v := range at {
+					if k, ok := v.(string); ok {
+						pick(ctx, res, argMap, k)
+					} else {
+						ctx.GetLogger().Errorf("object_pick got non string value %v in argument %v", v, args[1])
 					}
 				}
-				return res, true
-			default:
-				return fmt.Errorf("the argument should be slice or string"), false
-			}
-			for k, v := range argMap {
-				if sliceStringContains(pickArray, k) {
-					res[k] = v
+			case []string:
+				for _, v := range at {
+					pick(ctx, res, argMap, v)
+				}
+			case string:
+				pick(ctx, res, argMap, at)
+				for i := 2; i < len(args); i++ {
+					if k, ok := args[i].(string); ok {
+						pick(ctx, res, argMap, k)
+					} else {
+						ctx.GetLogger().Errorf("object_pick got non string argument %v", args[i])
+					}
 				}
 			}
-
 			return res, true
 		},
 		val: func(_ api.FunctionContext, args []ast.Expr) error {
-			return ValidateLen(2, len(args))
+			return ValidateAtLeast(2, len(args))
 		},
-		check: returnNilIfHasAnyNil,
 	}
 	builtins["obj_to_kvpair_array"] = builtinFunc{
 		fType: ast.FuncTypeScalar,
@@ -300,6 +314,38 @@ func registerObjectFunc() {
 		check: returnNilIfHasAnyNil,
 	}
 }
+
+func pick(ctx api.FunctionContext, res map[string]any, argMap map[string]any, k string) {
+	if !strings.Contains(k, ".") {
+		v, ok := argMap[k]
+		if ok {
+			res[k] = v
+		}
+		return
+	}
+	keys := strings.Split(k, ".")
+	r := argMap
+	var ok bool
+	for i := 0; i < len(keys)-1; i++ {
+		r, ok = r[keys[i]].(map[string]any)
+		if !ok {
+			ctx.GetLogger().Warnf("object_pick cannot pick key %s for %v", k, argMap)
+			return
+		}
+	}
+	v, ok := r[keys[len(keys)-1]]
+	if ok {
+		rr := res
+		for i := 0; i < len(keys)-1; i++ {
+			if rr[keys[i]] == nil {
+				rr[keys[i]] = make(map[string]any)
+				rr = rr[keys[i]].(map[string]any)
+			}
+		}
+		rr[keys[len(keys)-1]] = v
+	}
+}
+
 
 func sliceStringContains(s []string, target string) bool {
 	for _, v := range s {
