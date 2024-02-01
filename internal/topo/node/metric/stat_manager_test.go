1ca81a1f52b92b45611be79ec7526491aa59a38b
ngjaying <huangjy@emqx.io>
2024-02-01 14:09:50 +0800

    fix(stats): fix get metrics wrong pos (#2601)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ internal/topo/node/metric/stat_manager_test.go
@@ -0,0 +1,38 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metric
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	mockContext "github.com/lf-edge/ekuiper/internal/io/mock/context"
+)
+
+func TestGetMetrics(t *testing.T) {
+	ctx := mockContext.NewMockContext("rule1", "op1")
+	sm := NewStatManager(ctx, "op")
+	sm.ProcessTimeStart()
+	sm.IncTotalRecordsIn()
+	sm.SetBufferLength(20)
+	a := sm.GetMetrics()
+	e := []any{
+		int64(1), int64(0), int64(0), int64(0), int64(20), "current time", int64(0), "", 0,
+	}
+	assert.Equal(t, e[:5], a[:5])
+	assert.NotEqual(t, "", a[5])
+	assert.Equal(t, e[6:], a[6:])
+}
