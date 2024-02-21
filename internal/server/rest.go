7ad5783ee59f953a37359005584e3b9b7d54c24a
Song Gao <disxiaofei@163.com>
2024-02-21 12:22:10 +0800

    fix: fix rest and sql error and log (#2620) (#2634)
    Signed-off-by: yisaer <disxiaofei@163.com>

--- internal/server/rest.go
+++ internal/server/rest.go
@@ -1260,7 +1260,6 @@ func importRuleSetPartial(all processor.Ruleset) processor.Ruleset {
 			}
 			// Update to db after validation
 			_, err = ruleProcessor.ExecUpdate(k, v)
-
 			if err != nil {
 				ruleSetRsp.Rules[k] = err.Error()
 				continue
