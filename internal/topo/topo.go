3fcc5ab538d489c78630564102a547b198a7fb4d
ngjaying <huangjy@emqx.io>
2024-03-05 16:00:24 +0800

    fix(topo): avoid wrong topo open state (#2672)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/topo/topo.go
+++ internal/topo/topo.go
@@ -82,6 +82,7 @@ func (s *Topo) GetName() string {
 
 // Cancel may be called multiple times so must be idempotent
 func (s *Topo) Cancel() {
+	s.hasOpened.Store(false)
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	// completion signal
