a68535212c9a13593c8d84b68fee31a174abe3ae
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2024-03-26 23:32:53 -0400

    fix: fix comment language
    Signed-off-by: dovholuknf <46322585+dovholuknf@users.noreply.github.com>

--- internal/conf/logger/logger_adaptor.go
+++ internal/conf/logger/logger_adaptor.go
@@ -20,8 +20,8 @@ package logger
 import "github.com/sirupsen/logrus"
 
 // with EdgeX enabled as of 2024, it includes OpenZiti support. OpenZiti uses the default
-// logrus logger. This flag controls whether the logger should be quited or not and will
-// only be enabled when the -tags edgex is supplied to the build
+// logrus logger. This function controls whether the logger should be adapted. it will
+// only be enabled when the -tags edgex build tag is supplied to the build
 func adaptLogging(_ *logrus.Logger) {
 	//no operation by default
 }
