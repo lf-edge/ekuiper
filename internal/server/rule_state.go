418eb4cf9fb9a2756735c48676408988b7bfad57
yisaer <disxiaofei@163.com>
2024-03-27 10:49:51 +0800

    address the comment
    Signed-off-by: yisaer <disxiaofei@163.com>

---
+++ internal/server/rule_state.go
@@ -0,0 +1,78 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/lf-edge/ekuiper/pkg/cast"
+	"github.com/lf-edge/ekuiper/pkg/store"
+)
+
+type UpdateRuleStateType int
+
+const (
+	UpdateRuleSQLFieldValue UpdateRuleStateType = iota
+)
+
+func ruleStateHandler(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	vars := mux.Vars(r)
+	ruleID := vars["name"]
+	req := &ruleStateUpdateRequest{}
+	if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+		handleError(w, err, "", logger)
+		return
+	}
+	var err error
+	switch req.StateType {
+	case int(UpdateRuleSQLFieldValue):
+		err = updateRuleSQLValue(ruleID, req.Params)
+	default:
+		err = fmt.Errorf("unknown stateType:%v", req.StateType)
+	}
+	if err != nil {
+		handleError(w, err, "", logger)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("success"))
+}
+
+type ruleStateUpdateRequest struct {
+	StateType int                    `json:"type"`
+	Params    map[string]interface{} `json:"params"`
+}
+
+type updateFieldValueRequest struct {
+	StreamName string                 `json:"streamName"`
+	Input      map[string]interface{} `json:"input"`
+}
+
+func updateRuleSQLValue(ruleID string, param map[string]interface{}) error {
+	req := &updateFieldValueRequest{}
+	if err := cast.MapToStruct(param, req); err != nil {
+		return err
+	}
+	updated := store.GlobalWrapStore.UpdateIndexFieldValue(ruleID, req.StreamName, req.Input)
+	if updated {
+		return nil
+	}
+	return fmt.Errorf("rule:%v or stream:%v not found", ruleID, req.StreamName)
+}
