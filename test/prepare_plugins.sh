561f4d12f19d927607bc4a498a693671996a3975
Jiyong Huang <huangjy@emqx.io>
2024-04-17 08:41:05 +0800

    chore(temp): remove extensions
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- test/prepare_plugins.sh
+++ test/prepare_plugins.sh
@@ -28,86 +28,8 @@ chmod +x test/plugins/sql/create_table
 
 cd test
 
-rm -rf zmq.* Zmq.so
-
-FILE=../plugins/sources/Zmq.so
-if [ -f "$FILE" ]; then
-    echo "$FILE exists, not requried to build plugin."
-else
-    echo "$FILE does not exist, will build the plugin."
-    go build -trimpath --buildmode=plugin -o ../plugins/sources/Zmq.so ../extensions/sources/zmq/zmq.go
-fi
-
-mv ../plugins/sources/Zmq.so .
-cp ../extensions/sources/zmq/zmq.yaml .
-cp ../extensions/sources/zmq/zmq.json .
-zip zmq.zip Zmq.so zmq.yaml zmq.json
-rm -rf zmq.yaml Zmq.so
-
-rm -rf image.* Image.so
-
-FILE=../plugins/functions/Image.so
-if [ -f "$FILE" ]; then
-    echo "$FILE exists, not requried to build plugin."
-else
-    echo "$FILE does not exist, will build the plugin."
-    go build -trimpath --buildmode=plugin -o ../plugins/functions/Image.so ../extensions/functions/image/*.go
-fi
-
-mv ../plugins/functions/Image.so .
-cp ../extensions/functions/image/image.json .
-zip image.zip Image.so image.json
-rm -rf Image.so
-
-## build tdengine plugins
-#FILE=../plugins/sinks/Tdengine.so
-#if [ -f "$FILE" ]; then
-#    echo "$FILE exists, not requried to build plugin."
-#else
-#    echo "$FILE does not exist, will build the plugin."
-#    go build -trimpath --buildmode=plugin -o ../plugins/sinks/Tdengine.so ../extensions/sinks/tdengine/*.go
-#fi
-#
-#mv ../plugins/sinks/Tdengine.so .
-#mv ../extensions/sinks/tdengine/tdengine.json .
-#zip tdengine.zip Tdengine.so tdengine.json
-#rm -rf Tdengine.so
-
-# build sql plugins
-FILE=../plugins/sinks/Sql.so
-if [ -f "$FILE" ]; then
-    echo "$FILE exists, not required to build plugin."
-else
-    echo "$FILE does not exist, will build the plugin."
-    go build -trimpath --buildmode=plugin -o ../plugins/sinks/Sql.so ../extensions/sinks/sql/*.go
-fi
-
-mv ../plugins/sinks/Sql.so .
-cp ../extensions/sources/sql/sql.json .
-zip sql.zip Sql.so sql.json
-rm -rf Sql.so
-
-FILE=../plugins/sources/Sql.so
-if [ -f "$FILE" ]; then
-    echo "$FILE exists, not required to build plugin."
-else
-    echo "$FILE does not exist, will build the plugin."
-    go build -trimpath --buildmode=plugin -o ../plugins/sources/Sql.so ../extensions/sources/sql/*.go
-fi
-
-mv ../plugins/sources/Sql.so .
-cp yaml_for_test/sql.yaml .
-cp ../extensions/sources/sql/sql.json .
-zip sqlSrc.zip Sql.so sql.yaml sql.json
-rm -rf Sql.so
-
 rm -rf plugins/service/web/plugins/
 mkdir -p plugins/service/web/plugins/
-mv zmq.zip plugins/service/web/plugins/
-mv image.zip plugins/service/web/plugins/
-#mv tdengine.zip plugins/service/web/plugins/
-mv sql.zip plugins/service/web/plugins/
-mv sqlSrc.zip plugins/service/web/plugins/
 
 # prepare portable plugins
 cd ..
