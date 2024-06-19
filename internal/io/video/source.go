4de4cb8eed243781d12fe95ec1f285d24f50153b
Jiyong Huang <huangjy@emqx.io>
2024-06-19 10:47:21 +0800

    feat(io): restore video source
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ internal/io/video/source.go
@@ -0,0 +1,109 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package video
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net/url"
+	"os/exec"
+	"strings"
+	"time"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+
+	"github.com/lf-edge/ekuiper/contract/v2/api"
+	"github.com/lf-edge/ekuiper/v2/pkg/cast"
+)
+
+const FRAMENUMBER = 5
+
+type Source struct {
+	Url  string `json:"url"`
+	meta map[string]any
+}
+
+func (s *Source) Provision(ctx api.StreamContext, props map[string]any) error {
+	c := exec.Command("ffmpeg", "-version")
+	output, err := c.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("check ffmpeg failed, err:%v", err)
+	}
+	ctx.GetLogger().Infof("ffmpeg dependency check result: %s", string(output))
+	if strings.Contains(string(output), "ffmpeg version") {
+		ctx.GetLogger().Infof("ffmpeg dependency check success")
+	} else {
+		return errors.New("ffmpeg dependency check failed")
+	}
+	err = cast.MapToStruct(props, s)
+	if err != nil {
+		return err
+	}
+
+	if s.Url == "" {
+		return errors.New("url is empty")
+	}
+	_, err = url.ParseRequestURI(s.Url)
+	if err != nil {
+		return fmt.Errorf("url is invalid, err:%v", err)
+	}
+	s.meta = map[string]any{"url": s.Url}
+	return nil
+}
+
+func (s *Source) Close(_ api.StreamContext) error {
+	// do nothing
+	return nil
+}
+
+func (s *Source) Connect(_ api.StreamContext) error {
+	return nil
+}
+
+func (s *Source) Pull(ctx api.StreamContext, trigger time.Time, ingest api.BytesIngest, ingestError api.ErrorIngest) {
+	buf, err := s.readFrameAsJpeg(ctx)
+	if err != nil {
+		ingestError(ctx, err)
+	} else {
+		ingest(ctx, buf.Bytes(), s.meta, trigger)
+	}
+}
+
+func (s *Source) readFrameAsJpeg(ctx api.StreamContext) (*bytes.Buffer, error) {
+	ctx.GetLogger().Debugf("read frame at %v", time.Now())
+	buf := bytes.NewBuffer(nil)
+	err := s.readTo(ctx, buf)
+	if err != nil {
+		return nil, fmt.Errorf("read frame failed, err:%v", err)
+	}
+	return buf, nil
+}
+
+func (s *Source) readTo(ctx api.StreamContext, out io.Writer) error {
+	ctx.GetLogger().Debugf("read frame at %v", time.Now())
+	return ffmpeg.Input(s.Url).
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", FRAMENUMBER)}).
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(out).
+		Run()
+}
+
+func GetSource() api.Source {
+	return &Source{}
+}
+
+var _ api.PullBytesSource = &Source{}
