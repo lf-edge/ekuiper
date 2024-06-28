58b2aceb2562efbe047d1fdef35e4f2fdf990d37
Song Gao <disxiaofei@163.com>
2024-06-28 09:35:42 +0800

    fix: let prom metrics controlled by tag (#2956)
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- internal/server/promMetrics/prom_metrcis.go
+++ internal/server/promMetrics/prom_metrcis.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build prometheus || !core
+
 package promMetrics
 
 import "github.com/prometheus/client_golang/prometheus"
