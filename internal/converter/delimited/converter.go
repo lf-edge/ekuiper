0a15c760b055be971034ec3a8cedc9f517ecbb6f
Jiyong Huang <huangjy@emqx.io>
2024-07-11 18:40:42 +0800

    feat(converter): delimiter pass header
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/converter/delimited/converter.go
+++ internal/converter/delimited/converter.go
@@ -16,6 +16,7 @@ package delimited
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"sort"
 	"strconv"
@@ -29,7 +30,8 @@ import (
 
 type Converter struct {
 	Delimiter string   `json:"delimiter"`
-	Cols      []string `json:"cols"`
+	Cols      []string `json:"fields"`
+	HasHeader bool     `json:"hasHeader"`
 }
 
 func NewConverter(props map[string]any) (message.Converter, error) {
@@ -65,8 +67,14 @@ func (c *Converter) Encode(ctx api.StreamContext, d any) (b []byte, err error) {
 			}
 			sort.Strings(keys)
 			c.Cols = keys
+			if c.HasHeader {
+				hb := []byte(strings.Join(c.Cols, c.Delimiter))
+				sb.WriteString(c.Delimiter)
+				_ = binary.Write(sb, binary.BigEndian, uint32(len(hb)))
+				sb.Write(hb)
+				ctx.GetLogger().Infof("delimiter header %s", hb)
+			}
 		}
-
 		for i, v := range c.Cols {
 			if i > 0 {
 				sb.WriteString(c.Delimiter)
