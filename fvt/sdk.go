ba0684fdf6dd391dca7dc9de566d01768697ecea
ngjaying <huangjy@emqx.io>
2024-08-16 12:01:00 +0800

    test(fvt): add code based fvt (#3104)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ fvt/sdk.go
@@ -0,0 +1,89 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fvt
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+const ContentTypeJson = "application/json"
+
+type SDK struct {
+	baseUrl    *url.URL
+	httpClient *http.Client
+}
+
+func NewSdk(baseUrl string) (*SDK, error) {
+	u, err := url.Parse(baseUrl)
+	if err != nil {
+		return nil, err
+	}
+	return &SDK{baseUrl: u, httpClient: &http.Client{}}, nil
+}
+
+func (sdk *SDK) Get(command string) (resp *http.Response, err error) {
+	return http.Get(sdk.baseUrl.JoinPath(command).String())
+}
+
+func (sdk *SDK) CreateStream(streamJson string) (resp *http.Response, err error) {
+	return http.Post(sdk.baseUrl.JoinPath("streams").String(), ContentTypeJson, bytes.NewBufferString(streamJson))
+}
+
+func (sdk *SDK) DeleteStream(name string) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodDelete, sdk.baseUrl.JoinPath("streams", name).String(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	return sdk.httpClient.Do(req)
+}
+
+func (sdk *SDK) CreateRule(ruleJson string) (resp *http.Response, err error) {
+	return http.Post(sdk.baseUrl.JoinPath("rules").String(), ContentTypeJson, bytes.NewBufferString(ruleJson))
+}
+
+func (sdk *SDK) DeleteRule(name string) (resp *http.Response, err error) {
+	req, err := http.NewRequest(http.MethodDelete, sdk.baseUrl.JoinPath("rules", name).String(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	return sdk.httpClient.Do(req)
+}
+
+func (sdk *SDK) GetRulStatus(name string) (metrics map[string]any, err error) {
+	resp, err := http.Get(sdk.baseUrl.JoinPath("rules", name, "status").String())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = json.Unmarshal(body, &metrics)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	return
+}
