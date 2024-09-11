24d30c27a13af43999a8bf257a7bda6d1eaae599
siwuxie <cwl990110@163.com>
2024-09-12 00:13:33 +0800

    code style for mnist
    Signed-off-by: siwuxie <cwl990110@163.com>

--- extensions/functions/mnist/lib/Readme.md
+++ extensions/functions/mnist/lib/Readme.md
@@ -1,4 +1,8 @@
 
 
 
-TODO：add readme for onnx
+# ONNX
+
+This function only need onnx-runtime lib, and the lib is support by [ort](github.com/yalue/onnxruntime_go) ,so you can use it directly rather than install C API like Tensorflow Lite.
+
+Support onnxruntime.so is enough.
--- extensions/functions/mnist/mnist_test.go
+++ extensions/functions/mnist/mnist_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/lf-edge/ekuiper/contract/v2/api"
 	ort "github.com/yalue/onnxruntime_go"
 	_ "image/gif"
@@ -11,10 +10,8 @@ import (
 	"os"
 	"sync"
 	"testing"
-	"time"
 )
 
-// todo 测试文件仿照tf lite
 func Test_mnist_Exec(t *testing.T) {
 	type fields struct {
 		modelPath         string
@@ -62,7 +59,7 @@ func Test_mnist_Exec(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			f := &MnistFunc{
 				modelPath:         tt.fields.modelPath,
-				once:              tt.fields.once,
+				once:              sync.Once{},
 				inputShape:        tt.fields.inputShape,
 				outputShape:       tt.fields.outputShape,
 				sharedLibraryPath: tt.fields.sharedLibraryPath,
@@ -70,68 +67,11 @@ func Test_mnist_Exec(t *testing.T) {
 			}
 
 			out, got1 := f.Exec(tt.args.in0, tt.args.args)
-			
+
 			if !got1 {
 				t.Errorf("Exec() error = %v, wantErr %v", got1, tt.want1)
 			}
 			fmt.Println(out)
 		})
 	}
-}
-
-/*
-➜  mnist git:(torch_dev_swx) ✗ go test -v -cover
-=== RUN   Test_mnist_Exec
-=== RUN   Test_mnist_Exec/test1
-Output probabilities:
-  0: 1.350922
-  1: 1.149244
-  2: 2.231948
-  3: 0.826893
-  4: -3.473754
-  5: 1.200287
-  6: -1.185765
-  7: -5.960128
-  8: 4.764542
-  9: -2.345179
- probably a 8, with probability 4.764542
-
------------------------------
-true --- PASS: Test_mnist_Exec (0.03s)
-    --- PASS: Test_mnist_Exec/test1 (0.03s)
-PASS
-coverage: 58.7% of statements
-ok      github.com/lf-edge/ekuiper/v2/extensions/functions/mnist        0.030s
-
-
-
-*/
-
-func TestPic(t *testing.T) {
-	const TOPIC = "tfdmnist"
-
-	images := []string{
-		"img.png",
-		// 其他你需要的图像
-	}
-	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883")
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	for _, image := range images {
-		fmt.Println("Publishing " + image)
-		payload, err := os.ReadFile(image)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		if token := client.Publish(TOPIC, 0, false, payload); token.Wait() && token.Error() != nil {
-			fmt.Println(token.Error())
-		} else {
-			fmt.Println("Published " + image)
-		}
-		time.Sleep(1 * time.Second)
-	}
-	client.Disconnect(0)
 }
