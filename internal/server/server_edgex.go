0046f86907d6e67098b239833a8bd59e85c46fc5
ngjaying <huangjy@emqx.io>
2024-09-13 10:34:29 +0800

    feat: restore edgex openziti (#3192)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ internal/server/server_edgex.go
@@ -0,0 +1,41 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build edgex || full
+
+package server
+
+import (
+	"net"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/lf-edge/ekuiper/v2/internal/conf"
+	edgex_vault "github.com/lf-edge/ekuiper/v2/internal/edgex"
+)
+
+func init() {
+	newNetListener = newZitifiedNetListener
+}
+
+func newZitifiedNetListener(addr string, logger *logrus.Logger) (net.Listener, error) {
+	if conf.Config != nil && conf.Config.Basic.EnableOpenZiti == true {
+		logger.Info("using ListenMode 'zerotrust'")
+		ctx := edgex_vault.AuthenicatedContext(logger)
+		serviceName := "edgex.rules-engine"
+		return ctx.Listen(serviceName)
+	} else {
+		return newTcpListener(addr, logger)
+	}
+}
