7a8b47818648ec34e3b32d0052d8d41753a76d7a
Jiyong Huang <huangjy@emqx.io>
2024-10-31 11:58:17 +0800

    feat(func): support ext props
    - support props func
    - support use props in template
    
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

---
+++ pkg/props/props_conf.go
@@ -0,0 +1,62 @@
+// Copyright 2023-2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package props
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/lf-edge/ekuiper/v2/pkg/timex"
+)
+
+var SC = &StaticConf{props: map[string]string{}}
+
+type StaticConf struct {
+	sync.RWMutex
+	props map[string]string
+}
+
+func InitProps() {
+	envVars := os.Environ()
+	for _, envVar := range envVars {
+		pair := strings.SplitN(envVar, "=", 2)
+		key := pair[0]
+		value := pair[1]
+		if strings.HasPrefix(key, "KUIPER_PROPS_") {
+			shortKey := strings.ToLower(strings.TrimPrefix(key, "KUIPER_PROPS_"))
+			SC.Set(shortKey, value)
+		}
+	}
+}
+
+func (s *StaticConf) Get(propName string) (string, bool) {
+	switch propName {
+	case "et":
+		return strconv.FormatInt(timex.GetNowInMilli(), 10), true
+	default:
+		s.RLock()
+		defer s.RUnlock()
+		v, ok := s.props[propName]
+		return v, ok
+	}
+}
+
+func (s *StaticConf) Set(propName string, value string) {
+	s.Lock()
+	defer s.Unlock()
+	s.props[propName] = value
+}
---
+++ pkg/props/props_conf_test.go
@@ -0,0 +1,42 @@
+// Copyright 2024 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package props
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPropsConf(t *testing.T) {
+	err := os.Setenv("KUIPER_PROPS_VAR1", "value1")
+	require.NoError(t, err)
+	defer os.Unsetenv("KUIPER_PROPS_VAR1")
+
+	err = os.Setenv("KUIPER_PROPS_VIN", "value2")
+	require.NoError(t, err)
+	defer os.Unsetenv("KUIPER_PROPS_VIN")
+
+	InitProps()
+	v1, ok := SC.Get("var1")
+	require.True(t, ok)
+	require.Equal(t, "value1", v1)
+	_, ok = SC.Get("var2")
+	require.False(t, ok)
+	vin, ok := SC.Get("vin")
+	require.True(t, ok)
+	require.Equal(t, "value2", vin)
+}
