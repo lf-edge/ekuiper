dbce32d5a195cf1de949b3a6a4e29f0df0f3330d
Song Gao <disxiaofei@163.com>
2024-11-13 13:37:28 +0800

    feat: support auth request headers (#3376)
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- internal/pkg/httpx/http.go
+++ internal/pkg/httpx/http.go
@@ -17,7 +17,6 @@ package httpx
 import (
 	"bytes"
 	"crypto/tls"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -81,37 +80,6 @@ func Send(logger api.Logger, client *http.Client, bodyType string, method string
 	}
 	logger.Debugf("do request: %#v", req)
 	return client.Do(req)
-}
-
-func convertToMap(v interface{}, sendSingle bool) (map[string]interface{}, error) {
-	switch t := v.(type) {
-	case []byte:
-		r := make(map[string]interface{})
-		if err := json.Unmarshal(t, &r); err != nil {
-			if sendSingle {
-				return nil, fmt.Errorf("fail to decode content: %v", err)
-			} else {
-				r["result"] = string(t)
-			}
-		}
-		return r, nil
-	case map[string]interface{}:
-		return t, nil
-	case []map[string]interface{}:
-		r := make(map[string]interface{})
-		if sendSingle {
-			return nil, fmt.Errorf("invalid content: %v", t)
-		} else {
-			j, err := json.Marshal(t)
-			if err != nil {
-				return nil, err
-			}
-			r["result"] = string(j)
-		}
-		return r, nil
-	default:
-		return nil, fmt.Errorf("invalid content: %v", v)
-	}
 }
 
 func IsValidUrl(uri string) bool {
