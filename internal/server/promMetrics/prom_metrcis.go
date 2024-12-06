e23ecbffd00b3f1600f209287f32f17859cfde62
Song Gao <disxiaofei@163.com>
2024-12-06 16:07:34 +0800

    feat: remove promethues tag (#3427)
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- internal/server/promMetrics/prom_metrcis.go
+++ internal/server/promMetrics/prom_metrcis.go
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build prometheus || !core
-
 package promMetrics
 
 import "github.com/prometheus/client_golang/prometheus"
