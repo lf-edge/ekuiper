b9d9e653f77fcb49467dab842b4fe024ce9526ff
Jiyong Huang <huangjy@emqx.io>
2025-01-17 09:49:03 +0800

    fix(metric): avoid get proc panic in android
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- pkg/memory/mem.go
+++ pkg/memory/mem.go
@@ -1,4 +1,4 @@
-// Copyright 2023 EMQ Technologies Co., Ltd.
+// Copyright 2023-2025 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 package memory
 
 import (
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/mem"
 
 	"github.com/lf-edge/ekuiper/v2/internal/conf"
 	"github.com/lf-edge/ekuiper/v2/pkg/cgroup"
