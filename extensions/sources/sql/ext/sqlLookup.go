123b400936f21904b384821bf679f795f44f7b90
Song Gao <disxiaofei@163.com>
2025-02-28 15:17:59 +0800

    fix
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- extensions/sources/sql/ext/sqlLookup.go
+++ extensions/sources/sql/ext/sqlLookup.go
@@ -91,10 +91,10 @@ func (s *sqlLookupSource) Lookup(ctx api.StreamContext, fields []string, keys []
 		return nil, err
 	}
 	var result []api.SourceTuple
+	columns := make([]interface{}, len(cols))
+	prepareValues(ctx, columns, types, cols, nil)
 	for rows.Next() {
 		data := make(map[string]interface{})
-		columns := make([]interface{}, len(cols))
-		prepareValues(ctx, columns, types, cols, nil)
 		err := rows.Scan(columns...)
 		if err != nil {
 			return nil, err
