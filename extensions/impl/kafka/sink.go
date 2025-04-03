ccb805c48882865fe1effdc1607cad4ae6358857
Song Gao <disxiaofei@163.com>
2025-04-03 17:26:29 +0800

    fix(sink): check kafka batch config (#3651)
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- extensions/impl/kafka/sink.go
+++ extensions/impl/kafka/sink.go
@@ -159,6 +159,9 @@ func (k *KafkaSink) Provision(ctx api.StreamContext, configs map[string]any) err
 	if err != nil {
 		return err
 	}
+	if k.kc.BatchSize == 0 && k.kc.LingerInterval == 0 {
+		k.kc.BatchSize = 1
+	}
 	k.msgQ = make(chan *kafkago.Message, 2*k.kc.BatchSize)
 	// run batch
 	switch {
@@ -169,7 +172,6 @@ func (k *KafkaSink) Provision(ctx api.StreamContext, configs map[string]any) err
 	case k.kc.BatchSize == 0 && k.kc.LingerInterval > 0:
 		k.runWithTicker(ctx)
 	}
-
 	return nil
 }
 
