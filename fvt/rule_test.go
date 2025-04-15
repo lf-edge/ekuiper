93785a668b3ec3baf2926791f570f200250b7da7
ngjaying <huangjy@emqx.io>
2025-04-15 15:14:53 +0800

    fix(topo): subtopo ref count error when update  (#3670)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>
    (cherry picked from commit f28521960342420d0b57a11dc9db9fdbab29cf3a)

--- fvt/rule_test.go
+++ fvt/rule_test.go
@@ -15,9 +15,17 @@
 package fvt
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"sync"
 	"testing"
+	"time"
 
+	mqtt "github.com/mochi-mqtt/server/v2"
+	"github.com/mochi-mqtt/server/v2/hooks/auth"
+	"github.com/mochi-mqtt/server/v2/listeners"
+	"github.com/mochi-mqtt/server/v2/packets"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/lf-edge/ekuiper/v2/internal/io/memory/pubsub"
@@ -99,4 +107,120 @@ func (s *RuleTestSuite) assertRecvMemTuple(subCh chan any, expect []map[string]a
 		s.Require().Len(mt, 1)
 		s.Require().Equal(e, mt[0].ToMap())
 	}
+}
+
+func (s *RuleTestSuite) TestUpsert() {
+	topic := "sim/#"
+	server := mqtt.New(&mqtt.Options{InlineClient: true})
+	defer server.Close()
+	result := make(map[string]string)
+	lock := sync.Mutex{}
+	s.Run("start broker and subscribe for result", func() {
+		// Allow all connections.
+		_ = server.AddHook(new(auth.AllowHook), nil)
+		// Create a TCP listener on a standard port.
+		tcp := listeners.NewTCP(listeners.Config{ID: "upsert0", Address: ":4883"})
+		err := server.AddListener(tcp)
+		s.Require().NoError(err)
+		go func() {
+			err = server.Serve()
+			fmt.Println(err)
+		}()
+		fmt.Println(tcp.Address())
+		err = server.Subscribe(topic, 1, func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
+			lock.Lock()
+			defer lock.Unlock()
+			result[pk.TopicName] = string(pk.Payload)
+			if len(result) == 4 {
+				server.Unsubscribe(topic, 1)
+			}
+		})
+		s.Require().NoError(err)
+	})
+	s.Run("import initial rules and stop 1", func() {
+		oldContent, err := os.ReadFile("rules/old.json")
+		s.Require().NoError(err)
+		resp, err := client.Import(string(oldContent))
+		s.Require().NoError(err)
+		s.T().Log(GetResponseText(resp))
+		s.Require().Equal(http.StatusOK, resp.StatusCode)
+		time.Sleep(200 * time.Millisecond)
+		// check metrics
+		metrics, err := client.GetRuleStatus("hot")
+		s.Require().NoError(err)
+		s.Equal("running", metrics["status"])
+		s.T().Log(metrics)
+		sinkOut1, ok := metrics["sink_mqtt_0_0_records_out_total"]
+		s.True(ok)
+		s.Require().True(sinkOut1.(float64) > 10)
+		// Get 2nd metrics
+		metrics, err = client.GetRuleStatus("cold")
+		s.Require().NoError(err)
+		s.Equal("running", metrics["status"])
+		s.T().Log(metrics)
+		sinkOut2, ok := metrics["sink_mqtt_0_0_records_out_total"]
+		s.True(ok)
+		s.Require().True(sinkOut2.(float64) > 10)
+		// stop the cold rule
+		resp, err = client.StopRule("cold")
+		s.Require().NoError(err)
+		s.T().Log(GetResponseText(resp))
+		s.Require().Equal(http.StatusOK, resp.StatusCode)
+		time.Sleep(10 * time.Millisecond)
+		metrics, err = client.GetRuleStatus("cold")
+		s.Require().NoError(err)
+		s.Equal("stopped", metrics["status"])
+	})
+	s.Run("import two rules immediately", func() {
+		new1Content, err := os.ReadFile("rules/new1.json")
+		s.Require().NoError(err)
+		new2Content, err := os.ReadFile("rules/new2.json")
+		s.Require().NoError(err)
+		resp1, err1 := client.Import(string(new1Content))
+		resp2, err2 := client.Import(string(new2Content))
+		s.Require().NoError(err1)
+		s.Require().Equal(http.StatusOK, resp1.StatusCode)
+		s.Require().NoError(err2)
+		s.Require().Equal(http.StatusOK, resp2.StatusCode)
+		// wait and get the metrics
+		time.Sleep(200 * time.Millisecond)
+		// check metrics
+		metrics, err := client.GetRuleStatus("hot")
+		s.Require().NoError(err)
+		s.Equal("running", metrics["status"])
+		s.T().Log(metrics)
+		sinkOut1, ok := metrics["sink_mqtt_0_0_records_out_total"]
+		s.True(ok)
+		s.Require().True(sinkOut1.(float64) > 10)
+		connTime1, ok := metrics["source_simup_0_connection_last_connected_time"]
+		s.True(ok)
+		// Get 2nd metrics
+		metrics, err = client.GetRuleStatus("cold")
+		s.Require().NoError(err)
+		s.Equal("running", metrics["status"])
+		s.T().Log(metrics)
+		sinkOut2, ok := metrics["sink_mqtt_0_0_records_out_total"]
+		s.True(ok)
+		s.Require().True(sinkOut2.(float64) > 10)
+		connTime2, ok := metrics["source_simup_0_connection_last_connected_time"]
+		s.True(ok)
+		s.Require().Equal(connTime1, connTime2)
+	})
+	s.Run("clean", func() {
+		res, e := client.Delete("rules/cold")
+		s.NoError(e)
+		s.Equal(http.StatusOK, res.StatusCode)
+
+		res, e = client.Delete("rules/hot")
+		s.NoError(e)
+		s.Equal(http.StatusOK, res.StatusCode)
+
+		res, e = client.Delete("streams/simup")
+		s.NoError(e)
+		s.Equal(http.StatusOK, res.StatusCode)
+	})
+	s.Run("compare result", func() {
+		expected := map[string]string{"sim/new1": "{\"b\":2}", "sim/new2": "{\"a\":1}", "sim/old1": "{\"a\":1}", "sim/old2": "{\"b\":2}"}
+		s.Require().Equal(expected, result)
+	})
 }
