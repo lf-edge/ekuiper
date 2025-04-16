2c39edbe4803b21229c9ffc633a4b99fd141a677
Jiyong Huang <huangjy@emqx.io>
2025-04-16 15:15:17 +0800

    feat(enc): refactor enc pkg and add decryptor
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>
    (cherry picked from commit e979c6b62b133649a148f2e33ca10a7c9ff00f61)

--- modules/encryptor/encryptor.go
+++ modules/encryptor/encryptor.go
@@ -1,4 +1,4 @@
-// Copyright 2023-2024 EMQ Technologies Co., Ltd.
+// Copyright 2023-2025 EMQ Technologies Co., Ltd.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -18,23 +18,32 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/lf-edge/ekuiper/v2/internal/encryptor/aes"
+	"github.com/lf-edge/ekuiper/v2/modules/encryptor/aes"
 	"github.com/lf-edge/ekuiper/v2/pkg/message"
 )
 
-func GetEncryptor(name string, encryptProps map[string]any) (message.Encryptor, error) {
+func GetEncryptor(name string, key []byte, encryptProps map[string]any) (message.Encryptor, error) {
 	switch name {
 	case "aes":
-		return aes.GetEncryptor(encryptProps)
+		return aes.GetEncryptor(key, encryptProps)
 	default:
 		return nil, fmt.Errorf("encryptor '%s' is not supported", name)
 	}
 }
 
-func GetEncryptWriter(name string, output io.Writer) (io.Writer, error) {
+func GetEncryptWriter(name string, output io.Writer, key []byte) (io.Writer, error) {
 	// TODO support encryption props later
 	if name == "aes" {
-		return aes.GetEncryptWriter(output, nil)
+		return aes.GetEncryptWriter(output, key, nil)
 	}
 	return nil, fmt.Errorf("unsupported encryptor: %s", name)
+}
+
+func GetDecryptor(name string, key []byte, decryptProps map[string]any) (message.Decryptor, error) {
+	switch name {
+	case "aes":
+		return aes.GetDecryptor(key, decryptProps)
+	default:
+		return nil, fmt.Errorf("decryptor '%s' is not supported", name)
+	}
 }
