02fd8d3bde599ef7e608a8c1159f0284ff910750
Jiyong Huang <huangjy@emqx.io>
2025-06-05 10:01:45 +0800

    fix(topo): shared stream checkpoint broadcast
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>
    (cherry picked from commit 2ceeea6fc2d4564ad9d5c9660f628070c36997df)

--- internal/topo/node/node_test.go
+++ internal/topo/node/node_test.go
@@ -15,11 +15,17 @@
 package node
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/lf-edge/ekuiper/v2/internal/pkg/def"
+	"github.com/lf-edge/ekuiper/v2/internal/topo/checkpoint"
+	"github.com/lf-edge/ekuiper/v2/internal/xsql"
+	mockContext "github.com/lf-edge/ekuiper/v2/pkg/mock/context"
 )
 
 func TestOutputs(t *testing.T) {
@@ -33,4 +39,77 @@ func TestOutputs(t *testing.T) {
 	err = n.RemoveOutput("rule.4")
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(n.outputs))
+}
+
+func TestMultipleOutputsBroadcast(t *testing.T) {
+	ctx := mockContext.NewMockContext("multi", "op1")
+	n := newDefaultNode("test", &def.RuleOption{})
+	n.ctx = ctx
+	output1 := make(chan any, 10)
+	output2 := make(chan any, 10)
+	err := n.AddOutput(output1, "rule.1_test")
+	require.NoError(t, err)
+	err = n.AddOutput(output2, "rule.2_test")
+	require.NoError(t, err)
+	tc := []struct {
+		name string
+		data any
+	}{
+		{
+			name: "row broadcast",
+			data: &xsql.Tuple{
+				Ctx:       nil,
+				Emitter:   "test",
+				Message:   map[string]any{"a": 20},
+				Timestamp: time.UnixMilli(123456789),
+			},
+		},
+		{
+			name: "collection broadcast",
+			data: &xsql.WindowTuples{
+				Content: []xsql.Row{
+					&xsql.Tuple{
+						Ctx:       nil,
+						Emitter:   "test",
+						Message:   map[string]any{"a": 30},
+						Timestamp: time.UnixMilli(123456789),
+					},
+				},
+			},
+		},
+		{
+			name: "buffer data",
+			data: &checkpoint.BufferOrEvent{
+				Data: &xsql.Tuple{
+					Emitter:   "test2",
+					Message:   map[string]any{"a": 40},
+					Timestamp: time.UnixMilli(123456789),
+				},
+				Channel: "test2",
+			},
+		},
+	}
+	for _, tt := range tc {
+		t.Run(tt.name, func(t *testing.T) {
+			var result1, result2 any
+			wg := &sync.WaitGroup{}
+			wg.Add(3)
+			go func() {
+				defer wg.Done()
+				n.Broadcast(tt.data)
+			}()
+			go func() {
+				defer wg.Done()
+				result1 = <-output1
+			}()
+			go func() {
+				defer wg.Done()
+				result2 = <-output2
+			}()
+			wg.Wait()
+			assert.False(t, result1 == result2)
+			assert.Equal(t, tt.data, result1)
+			assert.Equal(t, tt.data, result2)
+		})
+	}
 }
