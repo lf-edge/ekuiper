558c1f9e2bb7578a416e473c3779db5fa941b64e
Jiyong Huang <huangjy@emqx.io>
2025-06-24 10:16:23 +0800

    fix(rule): start stopping rule infinite loop (#93)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>
    
    (cherry picked from commit 8615f38e97716e745a299c2115cbfa80a1f1ff0b)
    Signed-off-by: Jiyong Huang <huangjy@emqx.io>

--- internal/topo/rule/state.go
+++ internal/topo/rule/state.go
@@ -138,8 +138,14 @@ func (s *State) Validate() (*topo.Topo, error) {
 }
 
 func (s *State) transit(newState RunState, err error) {
+	chainAction := false
 	s.Lock()
-	defer s.Unlock()
+	defer func() {
+		s.Unlock()
+		if chainAction {
+			s.nextAction()
+		}
+	}()
 	s.currentState = newState
 	if err != nil {
 		s.lastWill = err.Error()
@@ -147,8 +153,10 @@ func (s *State) transit(newState RunState, err error) {
 	switch newState {
 	case Running:
 		s.lastStartTimestamp = timex.GetNowInMilli()
+		chainAction = true
 	case Stopped, StoppedByErr, ScheduledStop:
 		s.lastStopTimestamp = timex.GetNowInMilli()
+		chainAction = true
 	default:
 		// do nothing
 	}
@@ -258,7 +266,6 @@ func (s *State) GetStatusMap() map[string]any {
 // By check state, it assures only one Start function is running at any time. (thread safe)
 // regSchedule: whether need to handle scheduler. If call externally, set it to true
 func (s *State) Start() error {
-	defer s.nextAction()
 	s.logger.Debug("start RunState")
 	done := s.triggerAction(ActionSignalStart)
 	if done {
@@ -283,7 +290,6 @@ func (s *State) Start() error {
 }
 
 func (s *State) ScheduleStart() error {
-	defer s.nextAction()
 	s.logger.Debug("scheduled start RunState")
 	done := s.triggerAction(ActionSignalScheduledStart)
 	if done {
@@ -375,7 +381,6 @@ func (s *State) triggerAction(action ActionSignal) bool {
 // Stop run stop action or add the stop action to queue
 // regSchedule: whether need to handle scheduler. If call externally, set it to true
 func (s *State) Stop() {
-	defer s.nextAction()
 	s.logger.Debug("stop RunState")
 	done := s.triggerAction(ActionSignalStop)
 	if done {
@@ -393,7 +398,6 @@ func (s *State) Stop() {
 }
 
 func (s *State) ScheduleStop() {
-	defer s.nextAction()
 	s.logger.Debug("scheduled stop RunState")
 	done := s.triggerAction(ActionSignalScheduledStop)
 	if done {
