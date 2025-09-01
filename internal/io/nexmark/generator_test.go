1e6b9f64ed9a0edd9a08a08709452bd3c3f0184b
Song Gao <disxiaofei@163.com>
2025-09-01 15:58:24 +0800

    license
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- internal/io/nexmark/generator_test.go
+++ internal/io/nexmark/generator_test.go
@@ -1,3 +1,17 @@
+// Copyright 2025 EMQ Technologies Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package nexmark
 
 import (
@@ -130,9 +144,9 @@ func TestEventGenerator_GenStreamWithZeroQPS(t *testing.T) {
 func TestEventGenerator_Close(t *testing.T) {
 	ctx := context.Background()
 	generator := NewEventGenerator(ctx, 10, 100)
-	
+
 	generator.GenStream()
-	
+
 	var events []map[string]any
 	timeout := time.After(1 * time.Second)
 
