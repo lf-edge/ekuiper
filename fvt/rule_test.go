09818604b1971a831bcbabd3e58e6475e605f4de
Elia Renzoni <elia.renzoni03@gmail.com>
2026-05-26 09:07:11 +0200

    fix: add error check
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- fvt/rule_test.go
+++ fvt/rule_test.go
@@ -734,7 +734,8 @@ func (s *RuleTestSuite) TestShowScanTableContentWithErrors() {
 
 		devicesJSON, _ := json.Marshal(devicesData)
 		devicesPath := filepath.Join(dataLoc, "devices.json")
-		os.WriteFile(devicesPath, devicesJSON, 0o644)
+		err = os.WriteFile(devicesPath, devicesJSON, 0o644)
+		s.Require().NoError(err)
 		defer os.Remove(devicesPath)
 
 		streamSql := `{"sql": "CREATE STREAM simpleStream (id BIGINT, temperature FLOAT, humidity FLOAT) WITH (DATASOURCE=\"sensorStream\", TYPE=\"simulator\", FORMAT=\"json\")"}`
@@ -780,7 +781,8 @@ func (s *RuleTestSuite) TestShowScanTableContentWithErrors() {
 		dataLoc, err := conf.GetDataLoc()
 		s.Require().NoError(err)
 		devicesPath := filepath.Join(dataLoc, "devices.json")
-		os.WriteFile(devicesPath, []byte(``), 0o644)
+		err = os.WriteFile(devicesPath, []byte(``), 0o644)
+		s.Require().NoError(err)
 		defer os.Remove(devicesPath)
 
 		streamSql := `{"sql": "CREATE STREAM sensorStream (id BIGINT, temperature FLOAT, humidity FLOAT) WITH (DATASOURCE=\"sensorStream\", TYPE=\"simulator\", FORMAT=\"json\")"}`
@@ -841,7 +843,8 @@ func (s *RuleTestSuite) TestShowScanTableContent() {
 	}
 	devicesJSON, _ := json.Marshal(devicesData)
 	devicesPath := filepath.Join(dataLoc, "devices.json")
-	os.WriteFile(devicesPath, devicesJSON, 0o644)
+	err = os.WriteFile(devicesPath, devicesJSON, 0o644)
+	s.Require().NoError(err)
 	defer os.Remove(devicesPath)
 
 	streamSql := `{"sql": "CREATE STREAM sensorStream (id BIGINT, temperature FLOAT, humidity FLOAT) WITH (DATASOURCE=\"sensorStream\", TYPE=\"simulator\", FORMAT=\"json\")"}`
