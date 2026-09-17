4e555ede50440036e790bfdcc93d129bf3db3bdb
Song Gao <disxiaofei@163.com>
2026-09-17 12:13:32 +0800

    current
    Signed-off-by: Song Gao <disxiaofei@163.com>

--- extensions/impl/sql/source.go
+++ extensions/impl/sql/source.go
@@ -178,7 +178,7 @@ func (s *SQLSourceConnector) queryData(ctx api.StreamContext, rcvTime time.Time,
 	logger.Debugf("Query the database with %s", query)
 
 	queryStart := time.Now()
-	rows, err := s.conn.GetDB().Query(query)
+	rows, err := s.conn.GetDB().QueryContext(ctx, query)
 	failpoint.Inject("QueryErr", func() {
 		err = errors.New("QueryErr")
 	})
