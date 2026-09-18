08ff80de0b3f286877c0a8a89bf4337e27bc6e28
Song Gao <disxiaofei@163.com>
2026-09-18 14:38:59 +0800

    current
    Signed-off-by: Song Gao <disxiaofei@163.com>

---
+++ extensions/impl/sql/client/client_reconnect_test.go
@@ -0,0 +1,50 @@
+package client
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/cenkalti/backoff/v4"
+	"github.com/lf-edge/ekuiper/v2/pkg/errorx"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRetryReconnectRetriesIOErrors(t *testing.T) {
+	attempts := 0
+	err := retryReconnect(context.Background(), backoff.NewConstantBackOff(0), func(context.Context) error {
+		attempts++
+		if attempts < 3 {
+			return errorx.NewIOErr("database unavailable")
+		}
+		return nil
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, 3, attempts)
+}
+
+func TestRetryReconnectDoesNotRetryPermanentErrors(t *testing.T) {
+	attempts := 0
+	err := retryReconnect(context.Background(), backoff.NewConstantBackOff(0), func(context.Context) error {
+		attempts++
+		return errors.New("invalid database configuration")
+	})
+
+	require.EqualError(t, err, "invalid database configuration")
+	require.Equal(t, 1, attempts)
+}
+
+func TestRetryReconnectStopsWhenContextIsCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	attempts := 0
+	err := retryReconnect(ctx, backoff.NewConstantBackOff(time.Hour), func(context.Context) error {
+		attempts++
+		cancel()
+		return errorx.NewIOErr("database unavailable")
+	})
+
+	require.ErrorIs(t, err, context.Canceled)
+	require.Equal(t, 1, attempts)
+}
